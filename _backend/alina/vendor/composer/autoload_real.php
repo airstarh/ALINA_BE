6f0a56e6957cd71aac0d33fbeafcf56612fc23de
airstarh <qwerty123qwerty>
2017-05-17 09:18:39 +0300

    Illuminate Composer Update

--- _backend/alina/vendor/composer/autoload_real.php
+++ _backend/alina/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit7e716fb45a10427f793934939b043842
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit7e716fb45a10427f793934939b043842', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
