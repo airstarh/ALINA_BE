707ef25ed8ab57c2be6c4926cb50a8f06225f739
airstarh <qwerty123qwerty>
2017-12-07 20:08:28 +0300

    dev

--- _backend/alina/mvc/model/eloquent/eav/trait_entity.php
+++ _backend/alina/mvc/model/eloquent/eav/trait_entity.php
@@ -5,195 +5,189 @@ namespace alina\mvc\model\eloquent\eav;
 use alina\mvc\controller\egEav;
 use \alina\vendorExtend\illuminate\alinaLaravelCapsule as Dal;
 
-trait trait_entity
-{
-    public $cV = [];
-    public $cA = [];
+trait trait_entity {
+	public $cV = [];
+	public $cA = [];
 
-    #region Add Attribute
+	#region Add Attribute
 
-    public function eavAddAttribute($a)
-    {
-        //ToDo: Validate $a, prepare $a.
-        $a = toObject($a);
+	public function eavAddAttribute($a) {
+		//ToDo: Validate $a, prepare $a.
+		$a = toObject($a);
 
-        $oA             = new attr();
-        $oA->name_sys   = $a->name_sys;
-        $oA->name_human = $a->name_human;
-        $oA->val_table  = $a->val_table;
-        $oA->prepareModel();
-        $oA->save();
-        error_log(__FUNCTION__,0);
-        error_log('$oA',0);
-        error_log(json_encode($oA),0);
+		$oA             = new attr();
+		$oA->name_sys   = $a->name_sys;
+		$oA->name_human = $a->name_human;
+		$oA->val_table  = $a->val_table;
+		$oA->prepareModel();
+		$oA->save();
+		error_log(__FUNCTION__, 0);
+		error_log('$oA', 0);
+		error_log(json_encode($oA), 0);
 
-        $attributeId = $oA->{$oA->primaryKey};
-        $entityId    = $this->{$this->primaryKey};
-        $entityTable = $this->table;
+		$attributeId = $oA->{$oA->primaryKey};
+		$entityId    = $this->{$this->primaryKey};
+		$entityTable = $this->table;
 
-        $oEa            = new ea();
-        $oEa->attr_id   = $attributeId;
-        $oEa->ent_id    = $entityId;
-        $oEa->ent_table = $entityTable;
-        $oEa->quantity  = $a->quantity;
-        $oEa->order     = $a->order;
-        $oEa->prepareModel();
-        $oEa->save();
-        error_log('$oEa',0);
-        error_log(json_encode($oEa),0);
+		$oEa            = new ea();
+		$oEa->attr_id   = $attributeId;
+		$oEa->ent_id    = $entityId;
+		$oEa->ent_table = $entityTable;
+		$oEa->quantity  = $a->quantity;
+		$oEa->order     = $a->order;
+		$oEa->prepareModel();
+		$oEa->save();
+		error_log('$oEa', 0);
+		error_log(json_encode($oEa), 0);
 
-        return $this;
-    }
+		return $this;
+	}
 
-    #endregion Add Attribute
+	#endregion Add Attribute
 
-    #region Add Set Value
+	#region Add Set Value
 
-    public function eavSetValue($nameSys, $values)
-    {
-        $return = [];
-        $values = is_array($values) ? $values : [$values];
+	public function eavSetValue($nameSys, $values) {
+		$return = [];
+		$values = is_array($values) ? $values : [$values];
 
-        $oEaA      = $this->getEaA($nameSys)[0];
-        $ea_id     = $oEaA->ea_id;
-        $val_table = $oEaA->val_table;
+		$oEaA      = $this->getEaA($nameSys)[0];
+		$ea_id     = $oEaA->ea_id;
+		$val_table = $oEaA->val_table;
 
-        //ToDo: Make deletion more accurate.
-        $oV = new val();
-        $oV->setTable($val_table)->where('ea_id', $ea_id)->forceDelete();
-        unset($oV);
+		//ToDo: Make deletion more accurate.
+		$oV = new val();
+		$oV->setTable($val_table)->where('ea_id', $ea_id)->forceDelete();
+		unset($oV);
 
-        foreach ($values as $v) {
-            $oV = new val();
-            $oV->setTable($val_table);
-            $oV->ea_id = $ea_id;
-            $oV->val   = $v;
-            $oV->save();
-            $return[] = $oV;
-        }
-        unset($oV);
+		foreach ($values as $v) {
+			$oV = new val();
+			$oV->setTable($val_table);
+			$oV->ea_id = $ea_id;
+			$oV->val   = $v;
+			$oV->save();
+			$return[] = $oV;
+		}
+		unset($oV);
 
-        error_log(__FUNCTION__,0);
-        error_log(json_encode($return),0);
+		error_log(__FUNCTION__, 0);
+		error_log(json_encode($return), 0);
 
-        return $return;
-    }
+		return $return;
+	}
 
-    public function getEaA($attrSysNames = [], $forThisEntity = TRUE)
-    {
-        $attrSysNames = is_array($attrSysNames) ? $attrSysNames : [$attrSysNames];
-        $entityId     = $this->{$this->primaryKey};
-        $entityTable  = $this->table;
-        $oEaA         = new attr();
-        $q            = $oEaA
-            ->select(
-                'a.*',
-                'a.id as attr_id',
-                'ea.id as ea_id',
-                'ea.quantity',
-                'ea.order'
-            )
-            ->from('attr as a')
-            ->join('ea as ea', 'ea.attr_id', '=', 'a.id')
-            ->where('ea.ent_table', '=', $entityTable);
+	public function getEaA($attrSysNames = [], $forThisEntity = TRUE) {
+		$attrSysNames = is_array($attrSysNames) ? $attrSysNames : [$attrSysNames];
+		$entityId     = $this->{$this->primaryKey};
+		$entityTable  = $this->table;
+		$oEaA         = new attr();
+		$q            = $oEaA
+			->select(
+				'a.*',
+				'a.id as attr_id',
+				'ea.id as ea_id',
+				'ea.quantity',
+				'ea.order'
+			)
+			->from('attr as a')
+			->join('ea as ea', 'ea.attr_id', '=', 'a.id')
+			->where('ea.ent_table', '=', $entityTable);
 
-        if ($forThisEntity) {
-            $q = $q->where('ea.ent_id', '=', $entityId);
-        }
+		if ($forThisEntity) {
+			$q = $q->where('ea.ent_id', '=', $entityId);
+		}
 
-        if ($attrSysNames && !empty($attrSysNames)) {
-            $q = $q->whereIn('a.name_sys', $attrSysNames);
-        }
+		if ($attrSysNames && !empty($attrSysNames)) {
+			$q = $q->whereIn('a.name_sys', $attrSysNames);
+		}
 
-        $cEaA = $q->get();
+		$cEaA = $q->get();
 
-        return $cEaA;
-    }
+		return $cEaA;
+	}
 
-    #emdregion Add Set Value
+	#emdregion Add Set Value
 
-    #region Get Values
-    public function eavGetValues($attrSysNames = [], $whereArray = [])
-    {
-        $attrSysNames = is_array($attrSysNames) ? $attrSysNames : [$attrSysNames];
-        $entityId     = $this->{$this->primaryKey};
-        $entityTable  = $this->table;
-        $oAeA         = new attr();
-        $cEaA         = $this->getEaA($attrSysNames);
+	#region Get Values
+	public function eavGetValues($attrSysNames = [], $whereArray = []) {
+		$attrSysNames = is_array($attrSysNames) ? $attrSysNames : [$attrSysNames];
+		$entityId     = $this->{$this->primaryKey};
+		$entityTable  = $this->table;
+		$oAeA         = new attr();
+		$cEaA         = $this->getEaA($attrSysNames);
 
-        $sequenceValTableEaId = [];
-        foreach ($cEaA as $i => $mEaA) {
-            $sequenceValTableEaId[$mEaA->val_table][] = $mEaA->ea_id;
-        }
+		$sequenceValTableEaId = [];
+		foreach ($cEaA as $i => $mEaA) {
+			$sequenceValTableEaId[$mEaA->val_table][] = $mEaA->ea_id;
+		}
 
-        $arrValues = [];
-        foreach ($sequenceValTableEaId as $valTable => $eaIds) {
-            $oVal = new val();
-            $q    = $oVal
-                ->setTable($valTable)
-                ->whereIn('ea_id', $eaIds);
-            //ToDo: Add more WHERE abilities.
-            $cVal                 = $q->get();
-            $arrValues[$valTable] = $cVal;
-        }
+		$arrValues = [];
+		foreach ($sequenceValTableEaId as $valTable => $eaIds) {
+			$oVal = new val();
+			$q    = $oVal
+				->setTable($valTable)
+				->whereIn('ea_id', $eaIds);
+			//ToDo: Add more WHERE abilities.
+			$cVal                 = $q->get();
+			$arrValues[$valTable] = $cVal;
+		}
 
-        return $arrValues;
-    }
+		return $arrValues;
+	}
 
-    /**
-     * [
-     *  [atrSysName, =, value]
-     *  [atrSysName, <, value]
-     *  [atrSysName, LIKE, %value%]
-     *  [atrSysName, IN, [ARRAY] ]
-     * ]
-     */
-    public function eavGetAllWhere($whereArray)
-    {
-        $thisTableName = $this->getTable();
+	/**
+	 * [
+	 *  [atrSysName, =, value]
+	 *  [atrSysName, <, value]
+	 *  [atrSysName, LIKE, %value%]
+	 *  [atrSysName, IN, [ARRAY] ]
+	 * ]
+	 */
+	public function eavGetAllWhere($whereArray) {
+		$thisTableName = $this->getTable();
 
-        $q = Dal::table($thisTableName)
-            ->select("{$thisTableName}.*")
-            ->join("ea", "ea.ent_id", '=', "{$thisTableName}.id")
-            ->join("attr", "attr.id", '=', "ea.attr_id");
+		$q = Dal::table($thisTableName)
+			->select("{$thisTableName}.*")
+			->join("ea", "ea.ent_id", '=', "{$thisTableName}.id")
+			->join("attr", "attr.id", '=', "ea.attr_id");
 
-        $attrSysNames = [];
-        foreach ($whereArray as $where) {
-            $attrSysNames[] = $where[0];
+		$attrSysNames = [];
+		foreach ($whereArray as $where) {
+			$attrSysNames[] = $where[0];
 
-            $nameSys  = $where[0];
-            $operator = $where[1];
-            $compare  = $where[2];
+			$nameSys  = $where[0];
+			$operator = $where[1];
+			$compare  = $where[2];
 
-            switch ($operator) {
+			switch ($operator) {
 
-                case 'in':
-                case 'IN':
-                    $q->whereIn("{$nameSys}.val", $compare);
-                    break;
+				case 'in':
+				case 'IN':
+					$q->whereIn("{$nameSys}.val", $compare);
+					break;
 
-                case '=':
-                default:
-                    $q->where("{$nameSys}.val", $operator, $compare);
-                    break;
-            }
-        }
+				case '=':
+				default:
+					$q->where("{$nameSys}.val", $operator, $compare);
+					break;
+			}
+		}
 
-        $cEaA = $this->getEaA($attrSysNames, FALSE);
+		$cEaA = $this->getEaA($attrSysNames, FALSE);
 
-        foreach ($cEaA as $i => $mEaA) {
-            $t       = $mEaA->val_table;
-            $nameSys = $mEaA->name_sys;
-            $q->addSelect("{$nameSys}.val as {$nameSys}");
-            $q->join("{$t} as {$nameSys}", "{$nameSys}.ea_id", '=', "ea.id");
-        }
+		foreach ($cEaA as $i => $mEaA) {
+			$t       = $mEaA->val_table;
+			$nameSys = $mEaA->name_sys;
+			$q->addSelect("{$nameSys}.val as {$nameSys}");
+			$q->join("{$t} as {$nameSys}", "{$nameSys}.ea_id", '=', "ea.id");
+		}
 
-        error_log(__FUNCTION__,0);
-        error_log(json_encode($q->toSql()),0);
+		error_log(__FUNCTION__, 0);
+		error_log(json_encode($q->toSql()), 0);
 
-        $cEntity = $q->get();
+		$cEntity = $q->get();
 
-        return $cEntity;
-    }
-    #endregion Get Values
+		return $cEntity;
+	}
+	#endregion Get Values
 }
