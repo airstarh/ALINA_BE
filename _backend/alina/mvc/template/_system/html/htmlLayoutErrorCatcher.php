4550f3f0269b7e90b0e6a0c6257f44e5c9eef9cd
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2019-12-23 13:13:15 +0300

    Error handling

---
+++ _backend/alina/mvc/template/_system/html/htmlLayoutErrorCatcher.php
@@ -0,0 +1,43 @@
+<?php
+/** @var $data html */
+
+use alina\mvc\view\html;
+use alina\utils\Sys;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= \alina\app::getConfig('title'); ?></title>
+    <?= $data->js() ?>
+    <?= $data->css() ?>
+</head>
+<body>
+<div id="alina-body-wrapper">
+    <div class="container">
+        <h1>Oh! Error occured </h1>
+        <h2><a href="/">Go Home</a></h2>
+        <?= $data->messages(); ?>
+        <?= $data->content(); ?>
+
+        <!-- ToDo: Security! Delete on PROD.-->
+        <?php if (\alina\mvc\model\CurrentUser::obj()->isAdmin()) : ?>
+            <div>
+                <?php
+                $h1 = 'Alina Details';
+                print_r("<h1>{$h1}</h1>");
+                echo '<pre>';
+                print_r(Sys::SUPER_DEBUG_INFO());
+                echo '</pre>';
+                print_r(\alina\utils\Sys::reportSpentTime());
+
+                ?>
+            </div>
+        <?php endif; ?>
+
+    </div> <!-- /container -->
+</div>
+</body>
+</html>
