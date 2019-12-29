acdd757cd4e35238388435d66d7ae72bc79e6996
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2019-12-29 17:52:57 +0300

    replay js app

--- _backend/alina/utils/Sys.php
+++ _backend/alina/utils/Sys.php
@@ -99,11 +99,11 @@ class Sys
 
         }
 
-        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED_WITH'])) {
             return TRUE;
         }
 
-        if (strtolower(filter_input(INPUT_SERVER, 'HTTP_X_REQUESTED_WITH')) === 'xmlhttprequest') {
+        if (isset($_SERVER['HTTP_REQUESTED_WITH']) && !empty($_SERVER['HTTP_REQUESTED_WITH'])) {
             return TRUE;
         }
 
