78eb02e9989cdee5f284e030e1b9bc00376db662
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2020-06-01 05:58:10 +0300

    dev

--- _backend/alina/mvc/template/Tale/actionUpsert.php
+++ _backend/alina/mvc/template/Tale/actionUpsert.php
@@ -1,47 +1,44 @@
 <?php
 /** @var $data stdClass */
 ?>
-
-<div class="row no-gutters">
-    <div class="col-auto">
-        <div class="float-left mr-1 fixed-height-150px">
-            <a href="<?= $data->owner_emblem ?>" target="_blank">
-                <img src="<?= $data->owner_emblem ?>" width="150px" class="rounded-circle">
-            </a>
-        </div>
-
-        <a href="/auth/profile/<?= $data->owner_id ?>">
-            <?= $data->owner_firstname ?>
-            <?= $data->owner_lastname ?>
-        </a>
-
-        <br>
-        <a href="/tale/upsert/<?= $data->id ?>">
-            <?= \alina\utils\DateTime::toHumanDateTime($data->publish_at) ?>
-        </a>
+<div class="container p-0"><!---->
+    <div class="row no-gutters mt-2 mb-2">
+        <h1 class="notranslate col">
+            <a href="/#/tale/upsert/<?= $data->id ?>" class="btn btn-block text-left" style="background-color: #8F2DA8; color: #fff;">
+                <?= $data->header ?: '¯_(ツ)_/¯' ?></a></h1>
     </div>
-    <div class="col"><h2><?= $data->header ?></h2></div>
-</div>
-<div class="row no-gutters">
-    <div class="col">
-        <div class="ck-content">
-            <?= $data->body ?>
+    <div class="row no-gutters">
+        <div class="col mx-auto">
+            <div class="row no-gutters">
+                <div class="col-auto">
+                    <div class="fixed-height-150px"><a href="/#/auth/profile/<?= $data->owner_id ?>" class=""><img src="<?= $data->owner_emblem ?>" width="100px" class="rounded-circle"><!----></a></div>
+                </div>
+                <div class="notranslate col text-right">
+                    <a href="/#/auth/profile/<?= $data->owner_id ?>" class="btn btn-sm btn-primary text-left text-break mb-1"><?= $data->owner_firstname ?> <?= $data->owner_lastname ?></a>
+                    <br>
+                    <a href="/#/tale/upsert/<?= $data->id ?>" aria-current="page" class="btn btn-sm btn-info text-left mb-1 router-link-exact-active router-link-active"><?= \alina\utils\DateTime::toHumanDateTime($data->publish_at) ?></a>
+                    <br>
+                </div>
+            </div>
+            <div>
+                <div class="row no-gutters">
+                    <div class="col">
+                        <div class="ck-content">
+                            <div class="notranslate">
+                                <?= $data->body ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row no-gutters">
+                    <a class="col btn"
+                       style="background-color: #8F2DA8; color: #fff;"
+                       href="/#/tale/upsert/<?= $data->id ?>"
+                    >
+                        Go to comments
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
-
-<div class="row no-gutters">
-    <div class="col">
-        <div>
-        </div>
-    </div>
-</div>
-<div class="clearfix"></div>
-<div class="row no-gutters">
-    <a class="col btn btn-secondary"
-       href="/#/tale/upsert/<?= $data->id ?>"
-    >
-        Go to comments
-    </a>
-</div>
-
