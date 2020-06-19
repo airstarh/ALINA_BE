4754169e0586b76bbc97ceac9911b42d791bf3e1
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2020-06-20 01:16:39 +0300

    dev

--- _backend/alina/mvc/template/Tale/actionUpsert.php
+++ _backend/alina/mvc/template/Tale/actionUpsert.php
@@ -3,23 +3,35 @@
 ?>
 <div class="container p-0"><!---->
     <div class="row no-gutters mt-2 mb-2">
-        <h1 class="notranslate col">
-            <a href="/#/tale/upsert/<?= $data->id ?>" class="btn btn-block text-left" style="background-color: #8F2DA8; color: #fff;">
-                <?= $data->header ?: '¯_(ツ)_/¯' ?></a></h1>
+        <div class="col">
+            <h1 class="notranslate rounded p-2" style="background-color: #8F2DA8; color: #fff;">
+                <a href="/#/tale/upsert/<?= $data->id ?>" class="text-left text-light">
+                    <?= $data->header ?: '¯_(ツ)_/¯' ?>
+                </a>
+                <span>
+                    <a href="/#/tale/upsert/<?= $data->id ?>" aria-current="page" class="btn btn-sm btn-info text-left float-right mt-3"><?= \alina\utils\DateTime::toHumanDateTime($data->publish_at) ?></a>
+                </span>
+                <span class="clearfix"></span>
+            </h1>
+        </div>
     </div>
     <div class="row no-gutters">
         <div class="col mx-auto">
-            <div class="row no-gutters">
+
+            <div class="row no-gutters mt-2 mb-2">
                 <div class="col-auto">
-                    <div class="fixed-height-150px"><a href="/#/auth/profile/<?= $data->owner_id ?>" class=""><img src="<?= $data->owner_emblem ?>" width="100px" class="rounded-circle"><!----></a></div>
-                </div>
-                <div class="notranslate col text-right">
-                    <a href="/#/auth/profile/<?= $data->owner_id ?>" class="btn btn-sm btn-primary text-left text-break mb-1"><?= $data->owner_firstname ?> <?= $data->owner_lastname ?></a>
-                    <br>
-                    <a href="/#/tale/upsert/<?= $data->id ?>" aria-current="page" class="btn btn-sm btn-info text-left mb-1 router-link-exact-active router-link-active"><?= \alina\utils\DateTime::toHumanDateTime($data->publish_at) ?></a>
-                    <br>
+                    <span class="btn-secondary text-left text-nowrap badge-pill p-2">
+                        <a href="/#/auth/profile/<?= $data->owner_id ?>" class="fixed-height-150px">
+                            <img src="<?= $data->owner_emblem ?>" width="100px" class="rounded-circle">
+                        </a>
+                        <a href="/#/auth/profile/<?= $data->owner_id ?>"
+                           class="text-light"
+                        ><?= $data->owner_firstname ?> <?= $data->owner_lastname ?>
+                        </a>
+                    </span>
                 </div>
             </div>
+
             <div>
                 <div class="row no-gutters">
                     <div class="col">
