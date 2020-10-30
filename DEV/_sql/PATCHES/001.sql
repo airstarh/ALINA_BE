606cd3f6c56777dfcf635e897cda0d2e759e45a3
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2020-10-30 05:09:13 +0300

    -----

--- DEV/_sql/PATCHES/001.sql
+++ DEV/_sql/PATCHES/001.sql
@@ -13,20 +13,20 @@ ALTER TABLE `alina`.`notification`
 
 -- SECS
 ALTER TABLE `alina`.`watch_visit`
-    ADD COLUMN `ip` CHAR (46) NULL AFTER `ip_id`,
-    ADD COLUMN `browser_enc` VARCHAR (256) NULL AFTER `browser_id`;
+    ADD COLUMN `ip` CHAR(46) NULL AFTER `ip_id`,
+    ADD COLUMN `browser_enc` VARCHAR(256) NULL AFTER `browser_id`;
 
 ALTER TABLE `alina`.`watch_banned_visit`
     DROP COLUMN `ip_id`,
     DROP COLUMN `browser_id`,
-    ADD COLUMN `ip` CHAR (46) NULL AFTER `id`,
-    ADD COLUMN `browser_enc` VARCHAR (256) NULL AFTER `ip`,
-    CHANGE `reason` `reason` VARCHAR (300) CHARSET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT 'spam' NULL AFTER `browser_enc`;
+    ADD COLUMN `ip` CHAR(46) NULL AFTER `id`,
+    ADD COLUMN `browser_enc` VARCHAR(256) NULL AFTER `ip`,
+    CHANGE `reason` `reason` VARCHAR(300) CHARSET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT 'spam' NULL AFTER `browser_enc`;
 
-ALTER TABLE     `tale`
+ALTER TABLE `tale`
     ADD COLUMN `is_adult_denied` TINYINT DEFAULT 0 NULL AFTER `lang`,
     ADD COLUMN `is_draft` TINYINT DEFAULT 0 NULL AFTER `is_adult_denied`,
-    ADD COLUMN `editor_class` VARCHAR (100) DEFAULT 'ck-content' NULL AFTER `is_draft`,
+    ADD COLUMN `editor_class` VARCHAR(100) DEFAULT 'ck-content' NULL AFTER `is_draft`,
     ADD COLUMN `is_adv` TINYINT DEFAULT 0 NULL AFTER `editor_class`,
     ADD COLUMN `is_comment_denied` TINYINT DEFAULT 0 NULL AFTER `is_adv`;
 
