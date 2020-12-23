036241fc3437e745ebe2ff2911fe5a7170ad4bb8
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2020-12-23 14:39:17 +0300

    dev

--- DEV/bash/bash001.sh
+++ DEV/bash/bash001.sh
@@ -1,5 +1,11 @@
+##################################################
+#region RIGHTS
 sudo chown www-data:www-data /srv/backend -R
 sudo chown www-data:www-data /var/www/www-root/data/www/saysimsim.ru -R
-
+#####
 sudo chmod -R 755 /srv/backend
 sudo chmod -R 755 /var/www/www-root/data/www/saysimsim.ru
+#endregion RIGHTS
+##################################################
+
+
