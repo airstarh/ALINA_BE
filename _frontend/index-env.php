fc46205de6a3113a6d85d25a2d0dbe40a0aff671
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2021-07-27 03:02:04 +0300

    sys CFG env

--- _frontend/index-env.php
+++ _frontend/index-env.php
@@ -1,8 +1,8 @@
 <?php
-define('ALINA_ENV', 'host.home');
+define('ALINA_ENV', 'SAYSIMSIM_HOME');
 ##################################################
 switch (ALINA_ENV) {
-    case 'DA':
+    case 'SAYSIMSIM_HOME':
         define('ALINA_MODE', 'dev');
         define('ALINA_PATH_TO_FRAMEWORK', 'C:\_A001\REPOS\OWN\ALINA\_backend\alina');
         define('ALINA_PATH_TO_FRAMEWORK_CONFIG', 'C:\_A001\REPOS\OWN\ALINA\_backend\_CFG\alina\default.php');
@@ -10,15 +10,7 @@ switch (ALINA_ENV) {
         define('ALINA_PATH_TO_APP_CONFIG', 'C:\_A001\REPOS\OWN\ALINA\_backend\_CFG\apps\zero\default.php');
         define('ALINA_WEB_PATH', __DIR__);
         break;
-    case 'PROD':
-        define('ALINA_MODE', 'PROD1');
-        define('ALINA_PATH_TO_FRAMEWORK', '/srv/backend/alina');
-        define('ALINA_PATH_TO_FRAMEWORK_CONFIG', '/srv/backend/_CFG/alina/default.php');
-        define('ALINA_PATH_TO_APP', '/srv/backend/_aplications/zero');
-        define('ALINA_PATH_TO_APP_CONFIG', '/srv/backend/_CFG/apps/zero/default.php');
-        define('ALINA_WEB_PATH', __DIR__);
-        break;
-	case 'host.home':
+	case 'SAYSIMSIM_SERVER':
         define('ALINA_MODE', 'dev');
         define('ALINA_PATH_TO_FRAMEWORK', '/srv/alina/_backend/alina');
         define('ALINA_PATH_TO_FRAMEWORK_CONFIG', '/srv/alina/_backend/_CFG/alina/default.php');
