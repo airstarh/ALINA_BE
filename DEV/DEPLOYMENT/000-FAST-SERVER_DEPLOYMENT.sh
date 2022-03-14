7ffc14dcd4e8bbfd297c0e76742b724088c6a47e
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2022-03-14 09:55:45 +0300

    m45a

--- DEV/DEPLOYMENT/000-FAST-SERVER_DEPLOYMENT.sh
+++ DEV/DEPLOYMENT/000-FAST-SERVER_DEPLOYMENT.sh
@@ -6,11 +6,13 @@ ssh -i C:\_A001\STATICA\_SSH\127-0-0-1 sewa@saysimsim.ru
 sudo chown www-data:www-data /srv/alina/_backend -R
 sudo chown www-data:www-data /var/www/saysimsim.ru -R
 sudo chown www-data:www-data /var/www/vov -R
+sudo chown www-data:www-data /var/www/m45a -R
 #####
 # CHMOD
 sudo chmod -R 755 /srv/alina/_backend
 sudo chmod -R 755 /var/www/saysimsim.ru
 sudo chmod -R 755 /var/www/vov
+sudo chmod -R 755 /var/www/m45a
 #endregion RIGHTS
 ##################################################
 #region RESTART
