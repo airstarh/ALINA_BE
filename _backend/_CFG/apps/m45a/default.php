6d0ffd3916eb015ad59617bf2ddb3c383d520e74
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2022-12-06 01:07:30 +0300

    m45a - CFG from server

--- _backend/_CFG/apps/m45a/default.php
+++ _backend/_CFG/apps/m45a/default.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'appNamespace' => 'm45a',
-    'title'        => 'Миронова 45А. Официальный сайт',
+    'title'        => 'Миронова 45А',
     'description'  => 'Официальный сайт ТСН "ТСЖ Миронова 45 А"',
     'logVisitsToDb'       => TRUE,
     'db'                  => require_once(__DIR__ . DIRECTORY_SEPARATOR . 'db.php'),
