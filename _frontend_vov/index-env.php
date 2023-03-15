e98773c7efc0a47be1418993427bde2e7d2b9a7b
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2023-03-15 10:19:20 +0600

    index_env

--- _frontend_vov/index-env.php
+++ _frontend_vov/index-env.php
@@ -1,18 +1,9 @@
 <?php
-//define('ALINA_ENV', 'VOV_HOME');
-define('ALINA_ENV', 'VOV_SERVER');
+define('ALINA_ENV', 'vov');
 ##################################################
 switch (ALINA_ENV) {
-    case 'VOV_HOME':
-        define('ALINA_MODE', 'dev');
-        define('ALINA_PATH_TO_FRAMEWORK', 'C:\_A001\REPOS\OWN\ALINA\_backend\alina');
-        define('ALINA_PATH_TO_FRAMEWORK_CONFIG', 'C:\_A001\REPOS\OWN\ALINA\_backend\_CFG\alina\default.php');
-        define('ALINA_PATH_TO_APP', 'C:\_A001\REPOS\OWN\ALINA\_backend\_aplications\vov');
-        define('ALINA_PATH_TO_APP_CONFIG', 'C:\_A001\REPOS\OWN\ALINA\_backend\_CFG\apps\vov\default.php');
-        define('ALINA_WEB_PATH', __DIR__);
-        break;
-	case 'VOV_SERVER':
-        define('ALINA_MODE', 'dev');
+    case 'vov':
+        define('ALINA_MODE', 'PROD1');
         define('ALINA_PATH_TO_FRAMEWORK', '/srv/alina/_backend/alina');
         define('ALINA_PATH_TO_FRAMEWORK_CONFIG', '/srv/alina/_backend/_CFG/alina/default.php');
         define('ALINA_PATH_TO_APP', '/srv/alina/_backend/_aplications/vov');
