ae885d30eeda7ee374d8bf282fe0a7d1a8699bfb
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2023-04-01 03:30:24 +0600

    Box API composer

---
+++ _backend/alina/services/thirdPart/BoxApi/BoxService.php
@@ -0,0 +1,379 @@
+<?php
+
+namespace alina\services\thirdPart\BoxApi;
+
+use Exception;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Key;
+use Lcobucci\JWT\Signer\Rsa\Sha256;
+
+#endregion Attach all necessary sources
+#region Get URL for preview
+class BoxService
+{
+    ##################################################
+    #region FIELDS
+    private $_appConfig;
+    private $_accessToken;
+    private $tokenDelimiter = '|||';
+    #endregion FIELDS
+    ##################################################
+    #region INIT
+    public function __construct()
+    {
+        $this->_appConfig   = $this->getBoxApiConfig();
+        $this->_accessToken = $this->getAppUserAccessTokenFromBoxApi();
+    }
+
+    function getBoxApiConfig()
+    {
+        $DIR_STATIC = ALINA_PATH_TO_FRAMEWORK . '/services/thirdPart/BoxApi/static';
+
+        return [
+            'folder_id'                       => 0,
+            'access_token_storage'            => $DIR_STATIC . '/access-token-storage',
+            'access_token_storage_enterprise' => $DIR_STATIC . '/access-token-storage-enterprise',
+            'header'                          => [
+                // @link https://box-content.readme.io/v2.0/docs/app-auth
+                'alg' => 'RS256',    // The algorithm used to verify the signature. Values may only be set to: “RS256″, “RS384″, or “RS512.″
+                'typ' => 'JWT',      // Type of token. Default is “JWT” to specify a JSON Web Token (JWT).
+                'kid' => 'ejgrxup7', // Public Key ID
+            ],
+            // Claims Attributes
+            'claims'                          => [
+                'iss'            => 'yt193kvi5tm9jlmr6hpb4793wckn2qst', // The API key of the service that created the JWT assertion.
+                // For Enterprise
+                //'sub' => '911269', //enterprise_id for a token specific to an enterprise when creating and managing app users. app user_id for a token specific to an individual app user.
+                //'box_sub_type' => 'enterprise', // “enterprise” or “user” depending on the ID that was passed in the sub claim.
+                // For App User
+                // App User id
+                'sub'            => '271874469',
+                'box_sub_type'   => 'user',
+                // For the flexible switching between Enterprise and App User actions
+                'sub_enterprise' => '911269',                           // Enterprise ID
+                'sub_user'       => '271874469',                        // App User ID
+                'aud'            => 'https://api.box.com/oauth2/token',
+                'jti'            => base64_encode(random_bytes(32)), // A unique identifier specified by the client for this JWT. This is a unique string that is at least 16 characters and at most 128 characters.
+                'exp'            => time() + 60,                     // The unix time as to when this JWT will expire. This can be set to a maximum value of 60 seconds beyond the issue time. Note: It is recommended to set this value to less than the maximum allowed 60 seconds.
+                // Optional
+                'iat'            => time(),                          // Issued at time. The token cannot be used before this time.
+                'nbf'            => time() + 60,                     // Not before. Specifies when the token will start being valid.
+            ],
+            //'signature' => file_get_contents($DIR_STATIC.'/public_key.pem'),
+            'signature'                       => [
+                'public_key'  => file_get_contents($DIR_STATIC . '/public_key.pem'),
+                'private_key' => file_get_contents($DIR_STATIC . '/private_key.pem'),
+                'pass'        => 'qqqwwweee',
+            ],
+            'oauth_request'                   => [
+                'url'           => 'https://api.box.com/oauth2/token',
+                'grant_type'    => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+                'client_id'     => 'yt193kvi5tm9jlmr6hpb4793wckn2qst',
+                'client_secret' => 'hrRMWdoCeQXOHaWDFzO7zUPmNqGVdill',
+                'assertion'     => FALSE, // Parameter is set while runtime
+            ],
+        ];
+    }
+
+    function getAppUserAccessTokenFromBoxApi()
+    {
+        $boxApiConfig                           = $this->_appConfig; //$this->getBoxApiConfig();
+        $boxApiConfig['claims']['sub']          = $boxApiConfig['claims']['sub_user'];
+        $boxApiConfig['claims']['box_sub_type'] = 'user';
+
+        return $this->retrieveAccessTokenFromBoxApi($boxApiConfig);
+    }
+
+    function retrieveAccessTokenFromBoxApi($boxApiConfig = [])
+    {
+        error_log("retrieveAccessTokenFromBoxApi()", 0);
+        //error_log("  config=".json_encode($boxApiConfig),0);
+        $oauth_config = (isset($boxApiConfig) && !empty($boxApiConfig))
+            ? $boxApiConfig
+            : $this->getBoxApiConfig();
+        // If current token is valid, return current token.
+        $currentToken = $this->currentTokenIsValid($oauth_config['access_token_storage']);
+        error_log("  token=" . json_encode($currentToken), 0);
+        if ($currentToken) return $currentToken;
+        #region Generate JWT
+        $token = new Builder();
+        // Header
+        /*
+        $token->setHeader('alg', $oauth_config['header']['alg']);
+        $token->setHeader('typ', $oauth_config['header']['typ']);
+        $token->setHeader('kid', $oauth_config['header']['kid']);
+        */
+        $token
+            ->setIssuer($oauth_config['claims']['iss'])// Configures the issuer (iss claim)
+            ->setAudience($oauth_config['claims']['aud'])// Configures the audience (aud claim)
+            ->setId($oauth_config['claims']['jti'], TRUE)// Configures the id (jti claim), replicating as a header item
+            ->setExpiration($oauth_config['claims']['exp'])// Configures the expiration time of the token (exp claim)
+            ->set('sub', $oauth_config['claims']['sub'])// Configures a new claim, called "uid"
+            ->set('box_sub_type', $oauth_config['claims']['box_sub_type'])// Configures a new claim, called "uid"
+            ->set('alg', $oauth_config['header']['alg'])
+            ->set('typ', $oauth_config['header']['typ'])
+            ->set('kid', $oauth_config['header']['kid'])
+            // Optional parameters. @link https://box-content.readme.io/docs/app-auth
+            //->setIssuedAt($oauth_config['claims']['iat'])// Configures the time that the token was issue (iat claim)
+            //->setNotBefore($oauth_config['claims']['nbf'])// Configures the time that the token can be used (nbf claim)
+        ;
+        // Signature (Private Key)
+        $signer     = new Sha256();
+        $privateKey = new Key($oauth_config['signature']['private_key'], $oauth_config['signature']['pass']);
+        $token->sign($signer, $privateKey);
+        $JWT = $token->getToken();
+        //echo $JWT;
+        #endregion Generate JWT
+        #region Constructing the OAuth2 Request
+        $url     = $oauth_config['oauth_request']['url'];
+        $request = [
+            'grant_type'    => $oauth_config['oauth_request']['grant_type'],
+            'client_id'     => $oauth_config['oauth_request']['client_id'],
+            'client_secret' => $oauth_config['oauth_request']['client_secret'],
+            'assertion'     => $JWT,
+        ];
+        #endregion Constructing the OAuth2 Request
+        #region Send request to Box API
+        $response = $this->urlRequest($url, $request);
+        $response = json_decode($response);
+        //	echo '<pre>';
+        //	print_r($response);
+        //	echo '</pre>';die;
+        if (isset($response->error) && !empty($response->error)) {
+            error_log("  box login error {$response->error_description}", 0);
+            throw new Exception("BoxAPI Error: " . $response->error_description);
+        }
+        $access_token = (isset($response->access_token) && !empty($response->access_token))
+            ? $response->access_token
+            : 'Not Defined';
+        $expiresIn    = (isset($response->expires_in) && !empty($response->expires_in))
+            ? $response->expires_in
+            : 0;
+        $expiresAt    = $expiresIn + time();
+        // Write current token to file
+        file_put_contents($oauth_config['access_token_storage'], $access_token);
+        file_put_contents($oauth_config['access_token_storage'], $this->tokenDelimiter, FILE_APPEND);
+        file_put_contents($oauth_config['access_token_storage'], $expiresAt, FILE_APPEND);
+        #endregion Send request to Box API
+        error_log("  new token=" . json_encode($response), 0);
+
+        return $access_token;
+    }
+
+    function currentTokenIsValid($tokenStorage)
+    {
+        // Get expired-at time
+        $tokenString = file_get_contents($tokenStorage);
+        $tokenArray  = explode($this->tokenDelimiter, $tokenString);
+        //error_log("  token=".json_encode($tokenArray),0);
+        if (!isset($tokenArray[1]) || empty($tokenArray[1])) return FALSE;
+        $expiresAt = $tokenArray[1];
+        // Define validity
+        $time = time();
+        //error_log("  check expire={$expiresAt} now={$time}",0);
+        if ($expiresAt <= ($time - 10)/*seconds**/) {
+            //error_log("  expired",0);
+            return FALSE;
+        }
+
+        return $tokenArray[0]; // current access token
+    }
+
+    #endregion INIT
+    ##################################################
+    function retrieveBoxPreviewUrl($fileObj, $boxFolderId = NULL)
+    {
+        // @file api/boxApi/access-token-storage
+        //$this->getAppUserAccessTokenFromBoxApi();
+        $boxFolderId = $boxFolderId === NULL ? $this->_appConfig['folder_id'] : $boxFolderId;
+        // ToDo: It is possible to set a default 'Preview unavailable' URL;
+        $embedLink             = '';
+        $flagEmbedLinkReceived = FALSE;
+        if (isset($fileObj->box_id) && !empty($fileObj->box_id)) {
+            $boxFileId = $fileObj->box_id;
+            $response  = $this->getBoxEmbedUrl($boxFileId);
+            $response  = json_decode($response);
+            if (isset($response->expiring_embed_link->url) && !empty($response->expiring_embed_link->url)) {
+                $flagEmbedLinkReceived = TRUE;
+                $embedLink             = $response->expiring_embed_link->url;
+            }
+        }
+        //error_log(" link received? {$flagEmbedLinkReceived} link={$embedLink}", 0);
+        // When there is no file in Box storage
+        if (!$flagEmbedLinkReceived) {
+            $path = $fileObj->fullPath;
+            $file = $this->uploadFileToBox($path, $boxFolderId);
+            $file = json_decode($file);
+            if (!isset($file->entries[0]) || empty($file->entries[0]))
+                throw new Exception('File preview failed.');
+            $file      = $file->entries[0];
+            $boxFileId = $file->id;
+            $response  = $this->getBoxEmbedUrl($boxFileId);
+            $response  = json_decode($response);
+            if (!isset($response->expiring_embed_link->url) || empty($response->expiring_embed_link->url))
+                throw new Exception('File preview failed.');
+            $embedLink = $response->expiring_embed_link->url;
+            if (isset($fileObj->file_id) && !empty($fileObj->file_id)) {
+                dboUpdateByTableName(
+                    'file',
+                    ['box_id' => $boxFileId],
+                    'file_id',
+                    $fileObj->file_id
+                );
+            }
+        }
+
+        return $embedLink;
+    }
+    #endregion Get URL for preview
+    #region Access Tokens Retriever
+    // Enterprise
+    function getEnterpriseAccessTokenFromBoxApi()
+    {
+        $boxApiConfig                           = $this->_appConfig;  //$this->getBoxApiConfig();
+        $boxApiConfig['claims']['sub']          = $boxApiConfig['claims']['sub_enterprise'];
+        $boxApiConfig['claims']['box_sub_type'] = 'enterprise';
+        $boxApiConfig['access_token_storage']   = $boxApiConfig['access_token_storage_enterprise'];
+
+        return $this->retrieveAccessTokenFromBoxApi($boxApiConfig);
+    }
+
+    // User
+    #endregion Access Tokens Retriever
+    #region User Management
+    function createAppUser()
+    {
+        $this->getEnterpriseAccessTokenFromBoxApi();
+        $url      = 'https://api.box.com/2.0/users';
+        $user     = [
+            'name'                    => 'arena_' . time(),
+            'is_platform_access_only' => TRUE,
+        ];
+        $user     = json_encode($user);
+        $response = $this->urlRequest($url, $user, [$this->getAccessTokenHeaderEnterprise()]);
+        file_put_contents(__DIR__ . '/boxApi/app-users-storage', $response, FILE_APPEND);
+        file_put_contents(__DIR__ . '/boxApi/app-users-storage', PHP_EOL . PHP_EOL, FILE_APPEND);
+        echo $response;
+    }
+    #endregion User Management
+    #region helpers
+    function getBoxFolderObject($id = NULL)
+    {
+        /*
+         * @link https://box-content.readme.io/reference#folder-object
+         *
+         * curl https://api.box.com/2.0/folders/FOLDER_ID \
+         * -H "Authorization: Bearer ACCESS_TOKEN"
+         */
+        $id       = $id === NULL ? $this->_appConfig['folder_id'] : $id;
+        $url      = "https://api.box.com/2.0/folders/{$id}";
+        $response = $this->urlRequest($url, [], [$this->getAccessTokenHeader()]);
+
+        return $response;
+    }
+
+    function getFileSha1($path)
+    {
+        return sha1_file($path);
+    }
+
+    function uploadFileToBox($path, $boxFolderId = NULL)
+    {
+        error_log('uploadFileToBox path=' . $path, 0);
+        /*
+         * @link https://box-content.readme.io/reference#upload-a-file
+         *
+         * curl https://upload.box.com/api/2.0/files/content \
+         * -H "Authorization: Bearer ACCESS_TOKEN" -X POST \
+         * -F attributes='{"name":"tigers.jpeg", "parent":{"id":"11446498"}}' \
+         * -F file=@myfile.jpg
+         */
+        $boxFolderId = $boxFolderId === NULL ? $this->_appConfig['folder_id'] : $boxFolderId;
+        $realPath    = realpath($path);
+        if (!file_exists($realPath)) throw new Exception('No original file on server.');
+        // Preparations
+        $fileName = time() . '-' . basename($realPath);
+        $fileSha1 = $this->getFileSha1($realPath);
+        //$boxFolderObj = $this->getBoxFolderObject($boxFolderId); // Seems unnecessary
+        $attributes = [
+            'name'   => $fileName,
+            'parent' => [
+                'id' => $boxFolderId,
+            ],
+        ];
+        // Build Request
+        $url                 = 'https://upload.box.com/api/2.0/files/content';
+        $headers[]           = $this->getAccessTokenHeader();
+        $post['Content-MD5'] = $fileSha1;
+        $post['attributes']  = json_encode($attributes);
+        //$post['name'] = $fileName;
+        //$post['parent'] = $boxFolderObj;
+        //$post['id'] = $boxFolderId;
+        //$post['file'] = '@'.$realPath;
+        $post['file'] = new \CURLFile($realPath); // For PHP 5.6 only
+        $response     = $this->urlRequest($url, $post, $headers);
+
+        //error_log('  response='.json_encode($response), 0);
+        return $response;
+    }
+
+    function getBoxEmbedUrl($fileId)
+    {
+        /*
+         * @link https://box-content.readme.io/reference#get-embed-link
+         *
+         * curl https://api.box.com/2.0/files/FILE_ID?fields=expiring_embed_link \
+         * -H "Authorization: Bearer ACCESS_TOKEN"
+         *
+         */
+        $url       = "https://api.box.com/2.0/files/{$fileId}?fields=expiring_embed_link";
+        $headers[] = $this->getAccessTokenHeader();
+        $response  = $this->urlRequest($url, [], $headers);
+
+        return $response;
+    }
+
+    function urlRequest($url, $post = [], $headers = [])
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        // POST
+        if (!empty($post)) {
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        }
+        // Set Headers
+        if (!empty($headers)) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        }
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return $response;
+    }
+
+    function getAccessTokenHeader()
+    {
+        $accessToken = $this->_accessToken;
+
+        return "Authorization: Bearer {$accessToken}";
+    }
+
+    function getAccessTokenHeaderEnterprise()
+    {
+        $accessToken = $this->getAccessTokenEnterprise();
+
+        return "Authorization: Bearer {$accessToken}";
+    }
+
+    function getAccessTokenEnterprise()
+    {
+        $oauth_config = $this->_appConfig; //$this->getBoxApiConfig();
+        $tokenString  = file_get_contents($oauth_config['access_token_storage_enterprise']);
+        $tokenArray   = explode($this->tokenDelimiter, $tokenString);
+
+        return $tokenArray[0];
+    }
+}
+#endregion helpers
