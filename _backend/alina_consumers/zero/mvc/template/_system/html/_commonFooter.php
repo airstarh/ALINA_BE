ffd6ca56ba71066c6058d23fe0b8fee7131c5294
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2023-07-03 04:11:33 +0600

    ---

--- _backend/alina_consumers/zero/mvc/template/_system/html/_commonFooter.php
+++ _backend/alina_consumers/zero/mvc/template/_system/html/_commonFooter.php
@@ -6,24 +6,6 @@ use alina\utils\Sys;
 ?>
 <div class="row no-gutters mt-5 alina-form p-5">
     <div class="col">
-      Здесь люди просто тусят.
-
-      Коллектив индивидуалистов.
-
-      Славные ублюдки.
-
-      Идейная синь.
-
-      ОчУмелые ручки.
-
-      Лесная аристократия.
-
-      И прочие достойные персонажи.
-      <br>
-      <br>
-      Муза куража: <a href="https://www.facebook.com/sllimrut">Slimrut</a>
-      <br>
-      Ведущий проекта: <a href="https://vk.com/vsevolod.azovsky">Всеволод Азовский</a>
-      <br>
+        ZERO OREZ
     </div>
   </div>
