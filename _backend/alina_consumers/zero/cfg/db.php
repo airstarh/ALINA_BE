d5bbd83daca6eec3cae8ee49701c466c4ec223b2
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2023-07-03 04:15:41 +0600

    ---

---
+++ _backend/alina_consumers/zero/cfg/db.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'alina',
+    'username'  => 'root',
+    'password'  => 'root',
+    'charset'   => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix'    => '',
+    'port'      => 7107,
+];
