02437955d1117245f12a286d90a7f828cfab16c6
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2023-07-03 04:15:41 +0600

    ---

--- _backend/alina_consumers/zero/cfg/default.php
+++ _backend/alina_consumers/zero/cfg/default.php
@@ -3,6 +3,7 @@ return [
     'appNamespace' => 'zero',
     'title'        => 'ZERO',
     'description'  => 'OREZ',
+    'db'           => require_once(__DIR__ . DIRECTORY_SEPARATOR . 'db.php'),
     'mvc'          => [
         'defaultController'      => 'root',
         'defaultAction'          => 'index',
