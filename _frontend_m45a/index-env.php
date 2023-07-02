998851e965644407a51ff9049d4256edb90b5a6d
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2023-07-03 04:42:08 +0600

    ---

--- _frontend_m45a/index-env.php
+++ _frontend_m45a/index-env.php
@@ -1,14 +1,15 @@
 <?php
-define('ALINA_ENV', 'm45a');
 ##################################################
-switch (ALINA_ENV) {
-	case 'm45a':
-        define('ALINA_MODE', 'dev');
-        define('ALINA_PATH_TO_FRAMEWORK', '/srv/alina/_backend/alina');
-        define('ALINA_PATH_TO_FRAMEWORK_CONFIG', '/srv/alina/_backend/_CFG/alina/default.php');
-        define('ALINA_PATH_TO_APP', '/srv/alina/_backend/_aplications/m45a');
-        define('ALINA_PATH_TO_APP_CONFIG', '/srv/alina/_backend/_CFG/apps/m45a/default.php');
-        define('ALINA_WEB_PATH', __DIR__);
-        break;
-}
+#region HOST SPECIFIC
+const ALINA_WEB_PATH    = __DIR__;
+const ALINA_MODE        = 'DEV';
+const ALINA_BACKEND     = '/srv/php/_backend';
+const ALINA_PATH_TO_APP = ALINA_BACKEND . '/alina_consumers/m45a';
+#endregion HOST SPECIFIC
+##################################################
+#region AUTOMATIC
+const ALINA_PATH_TO_FRAMEWORK        = ALINA_BACKEND . '/alina';
+const ALINA_PATH_TO_FRAMEWORK_CONFIG = ALINA_PATH_TO_FRAMEWORK . '/cfg/default.php';
+const ALINA_PATH_TO_APP_CONFIG       = ALINA_PATH_TO_APP . '/cfg/default.php';
+#endregion AUTOMATIC
 ##################################################
--- _frontend_vov/index-env.php
+++ _frontend_vov/index-env.php
@@ -1,14 +1,15 @@
 <?php
-define('ALINA_ENV', 'vov');
 ##################################################
-switch (ALINA_ENV) {
-    case 'vov':
-        define('ALINA_MODE', 'PROD1');
-        define('ALINA_PATH_TO_FRAMEWORK', '/srv/alina/_backend/alina');
-        define('ALINA_PATH_TO_FRAMEWORK_CONFIG', '/srv/alina/_backend/_CFG/alina/default.php');
-        define('ALINA_PATH_TO_APP', '/srv/alina/_backend/_aplications/vov');
-        define('ALINA_PATH_TO_APP_CONFIG', '/srv/alina/_backend/_CFG/apps/vov/default.php');
-        define('ALINA_WEB_PATH', __DIR__);
-        break;
-}
+#region HOST SPECIFIC
+const ALINA_WEB_PATH    = __DIR__;
+const ALINA_MODE        = 'DEV';
+const ALINA_BACKEND     = '/srv/php/_backend';
+const ALINA_PATH_TO_APP = ALINA_BACKEND . '/alina_consumers/vov';
+#endregion HOST SPECIFIC
+##################################################
+#region AUTOMATIC
+const ALINA_PATH_TO_FRAMEWORK        = ALINA_BACKEND . '/alina';
+const ALINA_PATH_TO_FRAMEWORK_CONFIG = ALINA_PATH_TO_FRAMEWORK . '/cfg/default.php';
+const ALINA_PATH_TO_APP_CONFIG       = ALINA_PATH_TO_APP . '/cfg/default.php';
+#endregion AUTOMATIC
 ##################################################
