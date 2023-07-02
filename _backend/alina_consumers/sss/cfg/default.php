03e8d1fb599724ccbaa366d32be7a66f361c4d13
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2023-07-03 04:04:44 +0600

    ---

--- _backend/alina_consumers/sss/cfg/default.php
+++ _backend/alina_consumers/sss/cfg/default.php
@@ -1,13 +1,8 @@
 <?php
 return [
-    'appNamespace' => 'zero',
-    'title'        => '¯\_(ツ)_/¯',
-    'description'  => 'Коллектив индивидуалистовю  Идейная синь. Славные ублюдки. lucky fucky',
-    'mvc'          => [
-        'defaultController'      => 'root',
-        'defaultAction'          => 'index',
-        'pageNotFoundController' => 'root',
-        'pageNotFoundAction'     => '404',
-        'structure'              => [],
-    ],
+    'appNamespace'  => 'zero',
+    'title'         => '¯\_(ツ)_/¯',
+    'description'   => 'Коллектив индивидуалистовю. Идейная синь. Славные ублюдки. lucky fucky',
+    'logVisitsToDb' => TRUE,
+    'db'            => require_once(__DIR__ . DIRECTORY_SEPARATOR . 'db.php'),
 ];
