ea6d0b8e34b08e0e6d234c651de1ee954c0aaade
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2023-07-03 06:32:12 +0600

    ---

--- DEV/DEPLOYMENT/000-FAST-SERVER_DEPLOYMENT.sh
+++ DEV/DEPLOYMENT/000-FAST-SERVER_DEPLOYMENT.sh
@@ -1,26 +1,31 @@
 ##################################################
 ssh -i C:\_A001\STATICA\_SSH\127-0-0-1 sewa@saysimsim.ru
 ##################################################
-sudo chown www-data:www-data /srv -R
-sudo chmod -R 755 /srv
+#region /srv/
+sudo chown -R sewa /srv/
+sudo chgrp -R www-data /srv/
+sudo chmod -R 750 /srv/
+sudo chmod g+s /srv/
+#endregion /srv/
 ##################################################
-sudo usermod -a -G www-data sewa
+#region /var/www/
+sudo chown -R sewa /var/www/
+sudo chgrp -R www-data /var/www/
+sudo chmod -R 750 /var/www/
+sudo chmod g+s /var/www/
+#endregion /var/www/
 ##################################################
-#region RIGHTS
-# CHOWN
-sudo chown www-data:www-data /srv/alina/_backend -R
-sudo chown www-data:www-data /srv/php/_backend -R
-sudo chown www-data:www-data /var/www/saysimsim.ru -R
-sudo chown www-data:www-data /var/www/vov -R
-sudo chown www-data:www-data /var/www/m45a -R
-#####
-# CHMOD
-sudo chmod -R 755 /srv/alina/_backend
-sudo chmod -R 755 /srv/php/_backend
-sudo chmod -R 755 /var/www/saysimsim.ru
-sudo chmod -R 755 /var/www/vov
-sudo chmod -R 755 /var/www/m45a
-#endregion RIGHTS
+#region UPLOADS
+sudo chmod -R 770 /var/www/saysimsim.ru/uploads
+sudo chmod -R 770 /var/www/m45a/uploads
+sudo chmod -R 770 /var/www/vov/uploads
+#endregion UPLOADS
+##################################################
+
+
+
+
+
 ##################################################
 #region RESTART
 sudo systemctl restart nginx
@@ -29,13 +34,27 @@ sudo service mysql restart
 sudo service ssh restart
 #endregion RESTART
 ##################################################
-chown -R sewa /var/www/
-chgrp -R www-data /var/www/
-chmod -R 750 /var/www/
-chmod g+s /var/www/
+
+
+
+
+
 ##################################################
-chown -R sewa /srv/
-chgrp -R www-data /srv/
-chmod -R 750 /srv/
-chmod g+s /srv/
+# OLD:
 ##################################################
+sudo chown www-data:www-data /srv -R
+sudo chmod -R 755 /srv
+##################################################
+sudo usermod -a -G www-data sewa
+##################################################
+#region RIGHTS
+# CHOWN
+sudo chown www-data:www-data /srv/php/_backend -R
+sudo chown www-data:www-data /var/www -R
+#####
+# CHMOD
+sudo chmod -R 755 /srv/php/_backend
+sudo chmod -R 755 /var/www
+#endregion RIGHTS
+##################################################
+
