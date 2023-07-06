f0505f0a25312b83d9beb8384b6901358f1c2394
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2023-07-06 23:13:06 +0600

    ---

--- _backend/alina/appBoot.php
+++ _backend/alina/appBoot.php
@@ -8,21 +8,30 @@ class AlinaAdapter
 {
     static public function initiate()
     {
-        #####
+        ##################################################
+        #region COMMON FOR ALL
         define('ALINA_MICROTIME', $_SERVER['REQUEST_TIME_FLOAT'] ?: microtime(TRUE));
         define('ALINA_TIME', $_SERVER['REQUEST_TIME'] ?: time());
         define('ALINA_COOKIE_PAST', ALINA_TIME - 60 * 60);
         define('ALINA_MAX_TIME_DIFF_SEC', 48 * 60 * 60);
         define('ALINA_MIN_TIME_DIFF_SEC', 30);
         define('ALINA_AUTH_EXPIRES', ALINA_TIME + ALINA_MAX_TIME_DIFF_SEC);
-        #####
-        define('ALINA_MODE', 'dev');
-        define('ALINA_PATH_TO_FRAMEWORK', 'C:\_A001\REPOS\OWN\ALINA\_backend\alina');
-        define('ALINA_PATH_TO_FRAMEWORK_CONFIG', 'C:\_A001\REPOS\OWN\ALINA\_backend\_CFG\alina\default.php');
-        define('ALINA_PATH_TO_APP', ALINA_PATH_TO_FRAMEWORK);
-        define('ALINA_PATH_TO_APP_CONFIG', ALINA_PATH_TO_FRAMEWORK_CONFIG);
+        #endregion COMMON FOR ALL
+        ##################################################
+        #region HOST SPECIFIC
         define('ALINA_WEB_PATH', realpath($_SERVER['DOCUMENT_ROOT']));
-        define('ALINA_ENV', 'TEST');
+        define('ALINA_MODE', 'DEV');
+        #####
+        define("ALINA_BACKEND", 'C:/_A001/REPOS/OWN/ALINA/_backend');
+        define('ALINA_PATH_TO_APP', ALINA_BACKEND . '/alina');
+        #endregion HOST SPECIFIC
+        ##################################################
+        #region AUTOMATIC
+        define("ALINA_PATH_TO_FRAMEWORK", ALINA_BACKEND . '/alina');
+        define("ALINA_PATH_TO_FRAMEWORK_CONFIG", ALINA_PATH_TO_FRAMEWORK . '/cfg/default.php');
+        define("ALINA_PATH_TO_APP_CONFIG", ALINA_PATH_TO_APP . '/cfg/default.php');
+        #endregion AUTOMATIC
+        ##################################################
         require_once ALINA_PATH_TO_FRAMEWORK . DIRECTORY_SEPARATOR . 'App.php';
         $config = require(ALINA_PATH_TO_APP_CONFIG);
         $alina  = \alina\app::set($config);
