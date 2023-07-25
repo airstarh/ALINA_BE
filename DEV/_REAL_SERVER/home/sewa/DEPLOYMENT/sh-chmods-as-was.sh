d6a9630ed2ffb86fb4dab5c27633a8e2623018ec
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2023-07-25 14:39:09 +0600

    dev

---
+++ DEV/_REAL_SERVER/home/sewa/DEPLOYMENT/sh-chmods-as-was.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+##################################################
+#region RIGHTS
+# CHOWN
+sudo chown www-data:www-data /srv/alina/ -R
+sudo chmod -R 755 /srv/alina/
+#####
+sudo chown www-data:www-data /var/www/ -R
+sudo chmod -R 755 /var/www/
+#endregion RIGHTS
+##################################################
