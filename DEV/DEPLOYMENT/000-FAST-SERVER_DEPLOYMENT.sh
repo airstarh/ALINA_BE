9b6d57cbde371e884d4c2c4efc4add9a87a1b3d2
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2023-07-25 14:39:09 +0600

    dev

--- DEV/DEPLOYMENT/000-FAST-SERVER_DEPLOYMENT.sh
+++ DEV/DEPLOYMENT/000-FAST-SERVER_DEPLOYMENT.sh
@@ -1,31 +1,8 @@
 ##################################################
 ssh -i C:\_A001\STATICA\_SSH\127-0-0-1 sewa@saysimsim.ru
 ##################################################
-#region /srv/
-sudo chown -R sewa /srv/
-sudo chgrp -R www-data /srv/
-sudo chmod -R 750 /srv/
-sudo chmod g+s /srv/
-#endregion /srv/
-##################################################
-#region /var/www/
-sudo chown -R sewa /var/www/
-sudo chgrp -R www-data /var/www/
-sudo chmod -R 750 /var/www/
-sudo chmod g+s /var/www/
-#endregion /var/www/
-##################################################
-#region UPLOADS
-sudo chmod -R 770 /var/www/saysimsim.ru/uploads
-sudo chmod -R 770 /var/www/m45a/uploads
-sudo chmod -R 770 /var/www/vov/uploads
-#endregion UPLOADS
-##################################################
-
-
-
-
-
+cd /home/sewa/DEPLOYMENT
+sudo sh sh-chmods.sh
 ##################################################
 #region RESTART
 sudo systemctl restart nginx
@@ -34,27 +11,3 @@ sudo service mysql restart
 sudo service ssh restart
 #endregion RESTART
 ##################################################
-
-
-
-
-
-##################################################
-# OLD:
-##################################################
-sudo chown www-data:www-data /srv -R
-sudo chmod -R 755 /srv
-##################################################
-sudo usermod -a -G www-data sewa
-##################################################
-#region RIGHTS
-# CHOWN
-sudo chown www-data:www-data /srv/php/_backend -R
-sudo chown www-data:www-data /var/www -R
-#####
-# CHMOD
-sudo chmod -R 755 /srv/php/_backend
-sudo chmod -R 755 /var/www
-#endregion RIGHTS
-##################################################
-
