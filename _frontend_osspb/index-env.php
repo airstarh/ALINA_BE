4d8c1c90e086b469dade14e4d50922b3083675df
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2023-07-30 13:20:53 +0600

    Frontend osspb

---
+++ _frontend_osspb/index-env.php
@@ -0,0 +1,41 @@
+<?php
+##################################################
+#region HOST SPECIFIC
+const ALINA_WEB_PATH = __DIR__;
+const ALINA_ENV      = 'osspb';
+//const ALINA_ENV      = 'server001';
+const ALINA_MODE     = 'DEV';
+switch (ALINA_ENV) {
+    case 'server001':
+        define("ALINA_BACKEND", '/srv/php/_backend');
+        define('ALINA_PATH_TO_APP', ALINA_BACKEND . '/alina_consumers/zero');
+        break;
+    case 'zero':
+        define("ALINA_BACKEND", 'C:/_A001/REPOS/OWN/ALINA/_backend');
+        define('ALINA_PATH_TO_APP', ALINA_BACKEND . '/alina_consumers/zero');
+        break;
+    case 'vov':
+        define("ALINA_BACKEND", '/srv/alina/_backend');
+        define('ALINA_PATH_TO_APP', ALINA_BACKEND . '/alina_consumers/vov');
+        break;
+    case 'm45a':
+        define("ALINA_BACKEND", '/srv/alina/_backend');
+        define('ALINA_PATH_TO_APP', ALINA_BACKEND . '/alina_consumers/m45a');
+        break;
+    case 'sss':
+        define("ALINA_BACKEND", '/srv/alina/_backend');
+        define('ALINA_PATH_TO_APP', ALINA_BACKEND . '/alina_consumers/sss');
+        break;
+    case 'osspb':
+        define("ALINA_BACKEND", '/srv/php/_backend');
+        define('ALINA_PATH_TO_APP', ALINA_BACKEND . '/alina_consumers/osspb');
+        break;
+}
+#endregion HOST SPECIFIC
+##################################################
+#region AUTOMATIC
+const ALINA_PATH_TO_FRAMEWORK        = ALINA_BACKEND . '/alina';
+const ALINA_PATH_TO_FRAMEWORK_CONFIG = ALINA_PATH_TO_FRAMEWORK . '/cfg/default.php';
+const ALINA_PATH_TO_APP_CONFIG       = ALINA_PATH_TO_APP . '/cfg/default.php';
+#endregion AUTOMATIC
+##################################################
