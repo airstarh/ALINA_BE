547bd63c2ed0e6de51e27b147fb8a18a70901c6b
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2023-07-30 15:20:13 +0600

    osspb

---
+++ _backend/alina_consumers/osspb/cfg/db.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'driver'    => 'mysql',
+    'host'      => getenv('MYSQL_HOST_1'),
+    'database'  => 'home.osspb',
+    'username'  => 'root',
+    'password'  => getenv('MYSQL_ROOT_PASSWORD'),
+    'charset'   => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix'    => '',
+    'port'      => 3306,
+];
---
+++ _backend/alina_consumers/osspb/mvc/template/main/actionIndex.php
@@ -0,0 +1,59 @@
+<?php /** @var mixed $data */ ?>
+<div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col text-center">
+                <img src="/sources/images/logo.png" alt="© ООО “Отдел Сервис СПб” 2023" style="width: 50vmin">
+            </div>
+        </div>
+
+        <div class="m-1">&nbsp;</div>
+
+        <div class="row justify-content-center">
+            <div class="col text-center">
+                <h2 class="" style="font-size: 5vmin; font-weight: bold">
+                    © ООО “Отдел Сервис СПб” 2023
+                </h2>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col text-center">
+                Генподряд. Проектирование. Технический заказчик.
+            </div>
+        </div>
+
+        <div class="m-1">&nbsp;</div>
+
+        <!--<div class="row justify-content-center">-->
+        <!--    <div class="col-6 col-lg-3 text-right">-->
+        <!--        <b>Юридический адрес:</b>+-->
+        <!--    </div>-->
+        <!--    <div class="col-6 col-lg-3">-->
+        <!--        191119, г. Санкт-Петербург, ул. Звенигородская, дом 9-11, литер К, часть помещения 100-->
+        <!--    </div>-->
+        <!--</div>-->
+
+        <!--<div class="m-1">&nbsp;</div>-->
+
+        <div class="row justify-content-center">
+            <div class="col-sm-12 col-md-6 text-left text-md-right">
+                <b>Телефон:</b>
+            </div>
+            <div class="col text-nowrap text-right text-md-left">
+                <a href="tel:+78129067992">+7 (812) 906-79-92</a>
+            </div>
+        </div>
+
+        <div class="m-1">&nbsp;</div>
+
+        <div class="row justify-content-center">
+            <div class="col-sm-12 col-md-6 text-left text-md-right">
+                <b>Почта:</b>
+            </div>
+            <div class="col text-nowrap text-right text-md-left">
+                <a href="mailto:spb.otdelservice@gmail.com">spb.otdelservice@gmail.com</a>
+            </div>
+        </div>
+    </div>
+</div>
