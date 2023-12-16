a615a0fa8fcff87199cc4ab24f7efbcc756894a0
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2023-12-17 01:20:10 +0300

    ---

--- _DEV_DOCS/_CLI/sql/THE_STRINGS_SQL.sql
+++ _DEV_DOCS/_CLI/sql/THE_STRINGS_SQL.sql
@@ -5,3 +5,6 @@ CREATE INDEX audit_at_IDX USING BTREE ON `home.zero`.audit (`at` DESC);
 ALTER TABLE `home.zero`.tale DROP INDEX IND_TALE_PUBLISH_AT;
 ALTER TABLE `home.zero`.tale DROP INDEX IND_TALE_CREATED_AT;
 CREATE INDEX tale_publish_at_IDX USING BTREE ON `home.zero`.tale (publish_at DESC);
+
+ALTER TABLE `home.zero`.watch_visit DROP INDEX IND_WV_VISITED_AT;
+CREATE INDEX watch_visit_visited_at_IDX USING BTREE ON `home.zero`.watch_visit (visited_at DESC);
