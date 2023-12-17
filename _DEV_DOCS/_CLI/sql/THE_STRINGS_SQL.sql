1fa1ec608dd28595375464378c8c5505f9ed80ab
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2023-12-17 20:14:05 +0300

    sql

--- _DEV_DOCS/_CLI/sql/THE_STRINGS_SQL.sql
+++ _DEV_DOCS/_CLI/sql/THE_STRINGS_SQL.sql
@@ -1,10 +1,9 @@
 -- CREATE INDEX DESC
-CREATE INDEX audit_at_IDX USING BTREE ON `home.zero`.audit (`at` DESC);
+CREATE INDEX audit_at_IDX USING BTREE ON audit (`at` DESC);
 
 -- ALTER KEY
-ALTER TABLE `home.zero`.tale DROP INDEX IND_TALE_PUBLISH_AT;
-ALTER TABLE `home.zero`.tale DROP INDEX IND_TALE_CREATED_AT;
-CREATE INDEX tale_publish_at_IDX USING BTREE ON `home.zero`.tale (publish_at DESC);
-
-ALTER TABLE `home.zero`.watch_visit DROP INDEX IND_WV_VISITED_AT;
-CREATE INDEX watch_visit_visited_at_IDX USING BTREE ON `home.zero`.watch_visit (visited_at DESC);
+ALTER TABLE tale DROP INDEX IND_TALE_PUBLISH_AT;
+ALTER TABLE tale DROP INDEX IND_TALE_CREATED_AT;
+CREATE INDEX tale_publish_at_IDX USING BTREE ON tale (publish_at DESC);
+ALTER TABLE watch_visit DROP INDEX IND_WV_VISITED_AT;
+CREATE INDEX watch_visit_visited_at_IDX USING BTREE ON watch_visit (visited_at DESC);
