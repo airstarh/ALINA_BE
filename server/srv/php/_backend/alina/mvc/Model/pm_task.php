31b993e21f38f2bbc930830186be5a9484be03c3
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2023-12-22 21:41:36 +0300

    uask updates subtask

--- server/srv/php/_backend/alina/mvc/Model/pm_task.php
+++ server/srv/php/_backend/alina/mvc/Model/pm_task.php
@@ -114,7 +114,49 @@ class pm_task extends _BaseAlinaModel
                 ],
             ],
             ##### field #####
+            '_children'     => [
+                'has'        => 'many',
+                ##############################
+                # for Select With References
+                'joins'      => [
+                    ['join', 'pm_subtask AS pm_subtask', 'pm_subtask.pm_task_id', '=', "{$this->alias}.{$this->pkName}"],
+                ],
+                'conditions' => [],
+                'addSelects' => [
+                    [
+                        'addSelect',
+                        [
+                            'pm_subtask.id AS _pm_subtask_id',
+                            'pm_subtask.name_human AS _pm_subtask_name_human',
+                            'pm_subtask.time_estimated AS _pm_subtask_time_estimated',
+                            "{$this->alias}.{$this->pkName} AS main_id",
+                        ],
+                    ],
+                ],
+            ],
+            ##### field #####
         ];
+    }
+
+    public function hookRightAfterSave($data)
+    {
+        if (!AlinaAccessIfAdmin() && !AlinaAccessIfModerator()) {
+            return $this;
+        }
+
+        $this->bulkUpdate();
+
+        return $this;
+    }
+
+    public function bulkUpdate()
+    {
+        foreach ($this->attributes->_children as $child) {
+            $id = $child->_pm_subtask_id;
+            $m  = new pm_subtask();
+            $m->bulkUpdate($id);
+        }
+        return $this;
     }
     #####
 }
