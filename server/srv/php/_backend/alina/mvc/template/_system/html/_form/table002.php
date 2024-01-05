8afe5a200902fe6be2e46948aaf6dbe8b0465053
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2024-01-05 08:42:00 +0300

    ---

--- server/srv/php/_backend/alina/mvc/template/_system/html/_form/table002.php
+++ server/srv/php/_backend/alina/mvc/template/_system/html/_form/table002.php
@@ -33,7 +33,7 @@ $headers  = array_keys((array)$firstRow);
 
                     <?php if (!is_numeric($k)): ?>
                         <div>
-                            <?= $k ?>
+                            <?= ___($k) ?>
                         </div>
                     <?php else: ?>
                         <?= $counter++ ?>
@@ -57,7 +57,7 @@ $headers  = array_keys((array)$firstRow);
 
                         <td>
                             <?php if (Data::isIterable($colValue)) { ?>
-
+                                <div><?= $colName ?></div>
                                 <?= (new html)->piece('_system/html/_form/table002.php', $colValue) ?>
                             <?php } else { ?>
                                 <?= $colValue ?>
