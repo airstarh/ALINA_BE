33c4b4c2f105af739558f15acf7da2d4790bd786
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2024-01-07 07:31:14 +0300

    Pm - archive work done implementation

--- server/srv/php/_backend/alina/mvc/Model/pm_work_story.php
+++ server/srv/php/_backend/alina/mvc/Model/pm_work_story.php
@@ -187,40 +187,115 @@ class pm_work_story extends _BaseAlinaModel
         ];
     }
 
-    public function hookRightAfterSave()
+    public function boundModelsByWorkDoneId($idWorkDone)
     {
-        $this->updateWorkDone($this->id);
+        static $models = [];
+        if (
+            (isset($models['mW']))
+            &&
+            (isset($models['mWd']))
+            &&
+            (isset($models['mAssa']))
+        ) {
+            return $models;
+        }
+        $mWd   = new pm_work_done();
+        $mW    = new pm_work();
+        $mAssa = new user();
+        $mWd->getById($idWorkDone);
+        $mW->getById($mWd->attributes->pm_work_id);
+        $mW->getParents();
+        $mAssa->getById($mWd->attributes->assignee_id);
+        $models = [
+            'mWd'   => $mWd,
+            'mW'    => $mW,
+            'mAssa' => $mAssa,
+        ];
+        return $models;
     }
 
-    public function updateWorkDone($idWork)
-    {
-        $this->getById($idWork);
-        if ($this->attributes->flag_archived == 0) {
-            $m            = new pm_work_done();
-            $listWorkDone = $m
-                ->getAll([
-                    ['pm_work_id', '=', $idWork],
-                    ['flag_archived', '=', 0],
-                ])
-                ->toArray()
-            ;
-            if (!empty($listWorkDone)) {
-                $updated = [];
-                foreach ($listWorkDone as $item) {
-                    /**
-                     * Other staff happens in hookRightBeforeSave of pm_work_done
-                     */
-                    $updated[] = $item->id;
-                    (new pm_work_done())->updateById($item);
-                }
-                Message::setSuccess(implode(' ', [
-                    ___('Updated Done Works:'),
-                    count($updated),
-                ]));
-            }
-        }
 
+    public function doArchiveWorkDone($idWorkDone)
+    {
+        $models = $this->boundModelsByWorkDoneId($idWorkDone);
+        $mWd    = $models['mWd'];
+        $mW     = $models['mW'];
+        $mAssa  = $models['mAssa'];
+
+        $dataWorkStory = [
+            'pm_work_done_id'    => $mWd->attributes->id,
+            'wd_assignee_id'     => $mWd->attributes->assignee_id,
+            'pm_organization_id' => $mW->attributes->pm_organization->id,
+            'pm_department_id'   => $mW->attributes->pm_department->id,
+            'pm_project_id'      => $mW->attributes->pm_project->id,
+            'pm_task_id'         => $mW->attributes->pm_task->id,
+            'pm_subtask_id'      => $mW->attributes->pm_subtask->id,
+            'pm_work_id'         => $mW->attributes->id,
+        ];
+
+        $this->upsertByUniqueFields($dataWorkStory, [
+            'wd_assignee_id',
+            'pm_organization_id',
+            'pm_department_id',
+            'pm_project_id',
+            'pm_task_id',
+            'pm_subtask_id',
+            'pm_work_id',
+            'pm_work_done_id',
+        ]);
         return $this;
+    }
+
+
+    public function hookRightBeforeSave(&$dataArray)
+    {
+        $models = $this->boundModelsByWorkDoneId($dataArray['pm_work_done_id']);
+        $mWd    = $models['mWd'];
+        $mW     = $models['mW'];
+        $mAssa  = $models['mAssa'];
+        ###
+        $wd_for_date        = $mWd->attributes->for_date;
+        $wd_amount          = $mWd->attributes->amount;
+        $wd_price_final     = $mWd->attributes->price_final;
+        $wd_time_spent      = $mWd->attributes->time_spent;
+        $d_price_min        = $mW->attributes->pm_department->price_min;
+        $p_price_multiplier = $mW->attributes->pm_project->price_multiplier;
+        $st_time_estimated  = $mW->attributes->pm_subtask->time_estimated;
+        $w_price_this_work  = $mW->attributes->price_this_work;
+        ###
+        $d                       = [
+            'wd_for_date'        => $wd_for_date,
+            'd_price_min'        => $d_price_min,
+            'p_price_multiplier' => $p_price_multiplier,
+            'wd_amount'          => $wd_amount,
+            'wd_price_final'     => $wd_price_final,
+            'wd_time_spent'      => $wd_time_spent,
+            'w_price_this_work'  => $w_price_this_work,
+            'st_time_estimated'  => $st_time_estimated,
+        ];
+        $dataArray               = array_merge($dataArray, $d);
+        $name_human              = $this->calcNameHuman($dataArray);
+        $dataArray['name_human'] = $name_human;
+        return $this;
+    }
+
+    public function calcNameHuman($dataArray)
+    {
+        $models = $this->boundModelsByWorkDoneId($dataArray['pm_work_done_id']);
+        $mWd    = $models['mWd'];
+        $mW     = $models['mW'];
+        $mAssa  = $models['mAssa'];
+        ###
+        $d
+            = array_merge(
+            [
+                'assa_id'        => $mAssa->attributes->id,
+                'assa_firstname' => $mAssa->attributes->firstname,
+                'assa_lastname'  => $mAssa->attributes->lastname,
+                'assa_mail'      => $mAssa->attributes->mail,
+            ],
+            $dataArray);
+        return json_encode($d);
     }
     #####
 }
