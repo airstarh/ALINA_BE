b15a254b5151ff9238965812b0f2bd2a80036e01
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2024-01-09 09:27:45 +0300

    Pm Archived

--- server/srv/php/_backend/alina/mvc/template/Pm/_pmTablePerUser.php
+++ server/srv/php/_backend/alina/mvc/template/Pm/_pmTablePerUser.php
@@ -12,10 +12,31 @@ if (is_object($data)) $data = [$data];
 
 $counter = 1;
 
-$firstRow = current($data);
-$headers  = array_keys((array)$firstRow);
-
-$prevRow = [];
+//$firstRow = reset($data);
+//$headers  = array_keys((array)$firstRow);
+$GET            = \alina\Utils\Request::obj()->GET;
+$prevRow        = [];
+$wd_assignee_id = null;
+$headers        = [];
+$classArchived  = [];
+foreach ($data as $iR => &$r) {
+    foreach ($r as $f => &$v) {
+        $headers[$f] = $f;
+        switch ($f) {
+            case 'wd_assignee_id':
+                if ($f) $wd_assignee_id = $v;
+                break;
+            case 'wd_flag_archived':
+                $classArchived[$iR] = $v === 1 ? 'archived' : 'notarchived';
+                break;
+            default:
+                break;
+        }
+    }
+}
+?>
+<?php
+AlinaDebugJson($data);
 ?>
 <div class="mt-2 mb-5">
     <table class="bg-black alina-data-table alina-table-stick-header w-pct-100">
@@ -25,7 +46,28 @@ $prevRow = [];
         <tr>
             <th>#</th>
             <?php foreach ($headers as $h) { ?>
-                <th><?= ___($h) ?></th>
+
+                <th>
+                    <?php switch ($h): ?>
+<?php case 'wd_id': ?>
+                            <div>
+                                <form action="" method="post">
+                                    <input type="hidden" name="form_id" value="doArchiveAll">
+                                    <input type="hidden" name="do" value="doArchiveAll">
+                                    <input type="hidden" name="date_start" value="<?= $GET->date_start ?>">
+                                    <input type="hidden" name="date_end" value="<?= $GET->date_end ?>">
+                                    <input type="hidden" name="wd_assignee_id" value="<?= $wd_assignee_id ?>">
+                                    <button class="btn btn-sm btn-warning m-1"
+                                            type="submit"><?= ___('Archive All') ?></button>
+                                </form>
+                            </div>
+                            <?php break; ?>
+                        <?php default: ?>
+                            <?= ___($h) ?>
+                            <?php break; ?>
+                        <?php endswitch; ?>
+                </th>
+
             <?php } ?>
         </tr>
         </thead>
@@ -52,25 +94,20 @@ $prevRow = [];
         </tr>
         </tfoot>
 
-
+        <!--####################################################################################################-->
         <tbody>
         <?php foreach ($data as $idxRow => $row) { ?>
             <?php if (!is_numeric($idxRow)) continue; ?>
-            <tr>
+            <tr class="<?= $classArchived[$idxRow] ?>">
                 <td>
                     <?php if (!is_numeric($idxRow)): ?>
                         <div>
                             <?= ___($idxRow) ?>
                         </div>
                     <?php else: ?>
-                        <?= $counter++ ?>
+                        <div><?= $counter++ ?></div>
                     <?php endif; ?>
                 </td>
-
-                <?php
-                //AlinaDebugJson($k);
-                //AlinaDebugJson($row);
-                ?>
 
                 <?php if (!Data::isIterable($row)): ?>
                     <td>
@@ -94,9 +131,22 @@ $prevRow = [];
                                 <?php if (
                                     isset($prevRow[$colName])
                                     && !is_numeric($prevRow[$colName])
-                                    && $prevRow[$colName] == $colValue
+                                    && !Str::ifContains($colName, 'date')
+                                    && $prevRow[$colName] === $colValue
                                 ): ?>
-                                    ^
+
+                                <?php elseif ($colName === 'wd_flag_archived'): ?>
+                                    <?= $classArchived[$idxRow] ?>
+                                <?php elseif ($colName === 'wd_id'): ?>
+                                    <div>
+                                        <form action="" method="post">
+                                            <input type="hidden" name="form_id" value="doArchive">
+                                            <input type="hidden" name="do" value="doArchive">
+                                            <input type="hidden" name="wd_id" value="<?= $row['wd_id'] ?>">
+                                            <button class="btn btn-sm btn-warning"
+                                                    type="submit"><?= ___('Archive') ?></button>
+                                        </form>
+                                    </div>
                                 <?php else: ?>
                                     <?= $colValue ?>
                                 <?php endif; ?>
@@ -115,5 +165,6 @@ $prevRow = [];
         <?php } ?>
 
         </tbody>
+        <!--####################################################################################################-->
     </table>
 </div>
