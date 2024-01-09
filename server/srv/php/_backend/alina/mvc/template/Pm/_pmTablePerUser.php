32c5e2a9f89b1ea810c3952b9e0cd698fbe12c7b
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2024-01-09 04:22:48 +0300

    ---

--- server/srv/php/_backend/alina/mvc/template/Pm/_pmTablePerUser.php
+++ server/srv/php/_backend/alina/mvc/template/Pm/_pmTablePerUser.php
@@ -2,6 +2,7 @@
 
 use alina\mvc\View\html;
 use alina\Utils\Data;
+use alina\Utils\Str;
 
 if (empty($data)) {
     return;
@@ -16,8 +17,8 @@ $headers  = array_keys((array)$firstRow);
 
 $prevRow = [];
 ?>
-<div class="m-1">
-    <table class="bg-black alina-data-table alina-table-stick-header">
+<div class="mt-2 mb-5">
+    <table class="bg-black alina-data-table alina-table-stick-header w-pct-100">
 
 
         <thead>
@@ -66,14 +67,21 @@ $prevRow = [];
 
                     <?php foreach ($row as $colName => $colValue) { ?>
 
-                        <td>
+                        <td class="
+                                <?= Str::ifContains($colName, 'date') ? 'text-nowrap' : '' ?>
+                            "
+                        >
                             <?php if (Data::isIterable($colValue)) { ?>
                                 <div><?= $colName ?></div>
                                 <?= (new html)->piece('_system/html/_form/table002.php', $colValue) ?>
                             <?php } else { ?>
 
-                                <?php if (isset($prevRow[$colName]) && $prevRow[$colName] == $colValue): ?>
-
+                                <?php if (
+                                    isset($prevRow[$colName])
+                                    && !is_numeric($prevRow[$colName])
+                                    && $prevRow[$colName] == $colValue
+                                ): ?>
+                                 ^
                                 <?php else: ?>
                                     <?= $colValue ?>
                                 <?php endif; ?>
