3e1d2d3023f0e2ee13e2c4f714b119104c66aad4
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2024-01-15 13:57:49 +0300

    FORM

--- server/srv/php/_backend/alina/mvc/template/_system/html/_form/form.php
+++ server/srv/php/_backend/alina/mvc/template/_system/html/_form/form.php
@@ -5,10 +5,10 @@ use alina\mvc\View\html as htmlAlias;
 use alina\Utils\Data;
 use alina\Utils\Str;
 
-$action  = @$data->action ?: '';
-$enctype = @$data->enctype ?: 'multipart/form-data';
-$model   = $data->model;
-$sources = $data->sources;
+$action     = @$data->action ?: '';
+$enctype    = @$data->enctype ?: 'multipart/form-data';
+$attributes = $data->model;
+$sources    = $data->sources;
 
 ?>
 
@@ -16,79 +16,118 @@ $sources = $data->sources;
 
     <?= htmlAlias::elFormStandardButtons([]) ?>
 
-    <?php foreach ($model as $f => $v) { ?>
-        <?php
-        $_f = substr(strip_tags($f), 0, 200);
-        $_v = substr(strip_tags(Data::stringify($v)), 0, 200);
-        ?>
+    <?php foreach ($attributes
 
+                   as $f => $v) { ?>
+
+
+
+<?php
+$_f = substr(strip_tags($f), 0, 200);
+$_v = substr(strip_tags(Data::stringify($v)), 0, 200);
+
+$required = (isset($sources[$f]) && array_key_exists('required', $sources[$f]))
+    ? $sources[$f]['required']
+    : false;
+
+$disabled = (isset($sources[$f]) && array_key_exists('disabled', $sources[$f]))
+    ? $sources[$f]['disabled']
+    : false;
+
+$type = (isset($sources[$f]) && array_key_exists('type', $sources[$f]))
+    ? $sources[$f]['type']
+    : 'text';
+
+if (
+    \alina\Utils\Str::startsWith($_f, '_')
+    ||
+    \alina\Utils\Str::ifContains($_f, '.')
+) {
+    $type = 'readonly';
+}
+
+$flagList = array_key_exists($f, $sources) && array_key_exists('list', $sources[$f]);
+$multiple = (isset($sources[$f]) && isset($sources[$f]['multiple']) && $sources[$f]['multiple']);
+?>
+
+
+<?php if ($flagList) { ?>
+    <!--##################################################-->
+    <!--region SELECT-->
+
+    <?= htmlAlias::elFormSelect([
+        'multiple'    => $multiple,
+        'disabled'    => $disabled,
+        'required'    => $required,
+        'name'        => $f,
+        'value'       => (Data::isIterable($v)) ? (array)$v : [$v],
+        'options'     => $sources[$f]['list'],
+        'placeholder' => ___($f),
+    ]) ?>
+    <!--endregion SELECT-->
+    <!--##################################################-->
+
+<?php } elseif (Data::isIterable($v)) { ?>
+
+
+    <!--##################################################-->
+    <!--region Simple List-->
+    <?= htmlAlias::elBootstrapBadge([
+        'title' => $f,
+        'badge' => count((array)$v),
+    ]) ?>
+
+    <?= (new htmlAlias())->piece('_system/html/_form/table002.php', $v) ?>
+
+    <!--endregion Simple List-->
+    <!--##################################################-->
+
+
+<?php } else { ?>
+
+    <?php if ($type === 'readonly'): ?>
+
+        <div class="mt-3">&nbsp;</div>
+        <?= htmlAlias::elBootstrapBadge([
+        'title' => $_f,
+        'badge' => null,
+    ]) ?>
+
+        <?php if (Data::isStringValidJson($v)): ?>
+            <div>
+                <?= (new htmlAlias())->piece('_system/html/_form/table002.php', json_decode($v)) ?>
+            </div>
+        <?php else: ?>
+            <ul class="list-group">
+                <li class="list-group-item-dark d-flex justify-content-between align-items-center">
+                    <?= $_v ?>
+                </li>
+            </ul>
+        <?php endif; ?>
+    <?php else: ?>
         <!--##################################################-->
-        <!--region SELECT-->
-        <?php if (array_key_exists($f, $sources) && array_key_exists('list', $sources[$f])) { ?>
-            <?= htmlAlias::elFormSelect([
-                'multiple'    => (isset($sources[$f]['multiple'])) ? $sources[$f]['multiple'] : '',
-                'name'        => $f,
-                'value'       => (Data::isIterable($v)) ? (array)$v : [$v],
-                'options'     => $sources[$f]['list'],
-                'placeholder' => '¯\_(ツ)_/¯',//$f,
-            ]) ?>
-            <!--endregion SELECT-->
-            <!--##################################################-->
-
-            <!--##################################################-->
-            <!--region Simple List-->
-        <?php } elseif (Data::isIterable($v)) { ?>
-            <?= htmlAlias::elBootstrapBadge([
-                'title' => $f,
-                'badge' => count((array)$v),
-            ]) ?>
-
-            <?= (new htmlAlias)->piece('_system/html/_form/table002.php', $v) ?>
-
-            <!--endregion Simple List-->
-            <!--##################################################-->
-
-            <!--##################################################-->
-            <!--region Input Text-->
-        <?php } else { ?>
-            <?php
-            $type = (isset($sources[$f]) && array_key_exists('type', $sources[$f]))
-                ? $sources[$f]['type']
-                : 'text';
-            if (
-                \alina\Utils\Str::startsWith($_f, '_')
-                ||
-                \alina\Utils\Str::ifContains($_f, '.')
-            ) {
-                $type = 'readonly';
-            }
-            ?>
-
-            <?php if ($type === 'readonly') { ?>
-                <div class="mt-3">&nbsp;</div>
-                <?= htmlAlias::elBootstrapBadge([
-                'title' => $_f,
-                'badge' => null,
-            ]) ?>
-                <ul class="list-group">
-                    <li class="list-group-item-dark d-flex justify-content-between align-items-center">
-                        <?= $_v ?>
-                    </li>
-                </ul>
-
-            <?php } else { ?>
-                <?= htmlAlias::elFormInputText([
-                    'type'        => $type,
-                    'name'        => $f,
-                    'value'       => $v,
-                    'placeholder' => '',//$f,
-                ]) ?>
-
-            <?php } ?>
-        <?php } ?>
-        <!--endregion Input Text-->
+        <!--region Input Text/TextArea-->
+        <?= htmlAlias::elFormInputText([
+            'type'        => $type,
+            'required'    => $required,
+            'disabled'    => $disabled,
+            'name'        => $f,
+            'value'       => $v,
+            'placeholder' => ___($f),
+        ]) ?>
+        <!--endregion Input Text/TextArea-->
         <!--##################################################-->
-    <?php } ?>
+    <?php endif; ?>
+<?php } ?>
+
+
+
+<?php if ($disabled): ?>
+    <input type="hidden" name="<?= $f ?>" value="<?= $v ?>">
+<?php endif; ?>
+
+
+    <?php } ?><!--endforeach;-->
     <input type="hidden" name="form_id" value="actionEditRow">
     <?= htmlAlias::elFormStandardButtons([]) ?>
 </form>
