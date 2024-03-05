762b72e9bbda7ef6f4a80e465ba1c02127c0a663
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2024-03-05 22:01:13 +0300

    m45a links

--- server/srv/php/_backend/alina/Utils/Crypto.php
+++ server/srv/php/_backend/alina/Utils/Crypto.php
@@ -10,7 +10,7 @@ class Crypy
     }
     ##################################################
     #region Reversible
-    protected function get_ENCRYPTION_KEY()
+    protected function getKey()
     {
         $k              = [
             Request::obj()->IP,
@@ -21,24 +21,24 @@ class Crypy
         return $ENCRYPTION_KEY;
     }
 
-    public function revencr($plaintext, $ENCRYPTION_KEY = NULL)
+    public function encrypt($plaintext, $ENCRYPTION_KEY = null)
     {
         if (empty($ENCRYPTION_KEY)) {
-            $ENCRYPTION_KEY = $this->get_ENCRYPTION_KEY();
+            $ENCRYPTION_KEY = $this->getKey();
         }
         $ivlen          = openssl_cipher_iv_length($cipher = "AES-128-CBC");
         $iv             = openssl_random_pseudo_bytes($ivlen);
         $ciphertext_raw = openssl_encrypt($plaintext, $cipher, $ENCRYPTION_KEY, $options = OPENSSL_RAW_DATA, $iv);
-        $hmac           = hash_hmac('sha256', $ciphertext_raw, $ENCRYPTION_KEY, $as_binary = TRUE);
+        $hmac           = hash_hmac('sha256', $ciphertext_raw, $ENCRYPTION_KEY, $as_binary = true);
         $ciphertext     = base64_encode($iv . $hmac . $ciphertext_raw);
 
         return $ciphertext;
     }
 
-    public function revdecr($ciphertext, $ENCRYPTION_KEY = NULL)
+    public function decrypt($ciphertext, $ENCRYPTION_KEY = null)
     {
         if (empty($ENCRYPTION_KEY)) {
-            $ENCRYPTION_KEY = $this->get_ENCRYPTION_KEY();
+            $ENCRYPTION_KEY = $this->getKey();
         }
         $c              = base64_decode($ciphertext);
         $ivlen          = openssl_cipher_iv_length($cipher = "AES-128-CBC");
@@ -46,12 +46,12 @@ class Crypy
         $hmac           = substr($c, $ivlen, $sha2len = 32);
         $ciphertext_raw = substr($c, $ivlen + $sha2len);
         $plaintext      = openssl_decrypt($ciphertext_raw, $cipher, $ENCRYPTION_KEY, $options = OPENSSL_RAW_DATA, $iv);
-        $calcmac        = hash_hmac('sha256', $ciphertext_raw, $ENCRYPTION_KEY, $as_binary = TRUE);
+        $calcmac        = hash_hmac('sha256', $ciphertext_raw, $ENCRYPTION_KEY, $as_binary = true);
         if (hash_equals($hmac, $calcmac)) {
             return $plaintext;
         }
 
-        return FALSE;
+        return false;
     }
     #endregion Reversible
     ##################################################
