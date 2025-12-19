902847c3e4f7817d8a56adb65cdca4d17ae6b214
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2025-12-20 01:54:08 +0300

    unit tests beginning

--- server/srv/php/_backend/alina/tests/Alina001Test.php
+++ server/srv/php/_backend/alina/tests/Alina001Test.php
@@ -13,12 +13,33 @@
 require_once __DIR__ . '/../AppBoot.php';
 //require_once  __DIR__.'/../vendor/autoload.php';
 
+use alina\mvc\Model\user;
 use PHPUnit\Framework\TestCase;
 
 final class Alina001Test extends TestCase
 {
+    public function testZero()
+    {
+        echo PHP_EOL . __FUNCTION__;
+
+        $this->assertIsNumeric(time());
+    }
+
+    public function testDb()
+    {
+        echo PHP_EOL . __FUNCTION__;
+
+        $user = new user();
+        $user->getOne([]);
+
+        $this->assertIsNumeric($user->id);
+        $this->assertNotEmpty($user->id);
+    }
+
     public function testMergeSimpleObjects()
     {
+        echo PHP_EOL . __FUNCTION__;
+
         $this->assertInstanceOf(
             \stdClass::class,
             \alina\Utils\Data::mergeObjects([], [])
