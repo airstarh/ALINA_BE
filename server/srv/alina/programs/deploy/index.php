8550871141d4b4438d46f0abc4cb964593c3644f
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2025-12-21 21:09:42 +0300

    dev

--- server/srv/alina/programs/deploy/index.php
+++ server/srv/alina/programs/deploy/index.php
@@ -5,10 +5,9 @@ require_once __DIR__ . '/../../AppBoot.php';
 
 $projectList = [
     # 'from' => 'to',
-    #'/srv/alina_consumers/zero.home' => '/var/www/zero.home',
-    '/srv/alina_consumers/zero.home/.WwwDiff' => '/srv/testCopySmart/sewa',
+    '/srv/alina_consumers/zero.home/.WwwDiff' => '/var/www/zero.home',
 ];
 
 foreach ($projectList as $from => $to) {
-    FS::copySmart($from, $to);
+    FS::copySmart($from, $to, true);
 }
