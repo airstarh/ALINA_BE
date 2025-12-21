e95aaf0ac0ec467f6881c631e2ce75ee79a55624
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2025-12-21 21:51:57 +0300

    dev

--- server/srv/alina/programs/deploy/index.php
+++ server/srv/alina/programs/deploy/index.php
@@ -4,10 +4,32 @@ use alina\Utils\FS;
 require_once __DIR__ . '/../../AppBoot.php';
 
 $projectList = [
-    # 'from' => 'to',
-    '/srv/alina_consumers/zero.home/.WwwDiff' => '/var/www/zero.home',
+    // ZERO
+    '/var/www/zero.home' => [
+        '/srv/alina_consumers/zero.home/.WwwDiff',
+    ],
+
+    // SAYSIMSIM.RU
+    '/var/www/saysimsim.ru' => [
+        '/srv/alina_consumers/zero.home/.WwwDiff',
+        '/srv/alina_consumers/saysimsim.ru/.WwwDiff',
+    ],
+
+    // vov
+    '/var/www/vov' => [
+        '/srv/alina_consumers/zero.home/.WwwDiff',
+        '/srv/alina_consumers/vov/.WwwDiff',
+    ],
+
+    // m45a
+    '/var/www/m45a' => [
+        '/srv/alina_consumers/zero.home/.WwwDiff',
+        '/srv/alina_consumers/m45a/.WwwDiff',
+    ],
 ];
 
-foreach ($projectList as $from => $to) {
-    FS::copySmart($from, $to, true);
+foreach ($projectList as $to => $wwwDiff) {
+    foreach ($wwwDiff as $from) {
+        FS::copySmart($from, $to, true);
+    }
 }
