dbef82cc06c09e18a5ef74d5159788e6b30fa397
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2025-12-26 05:48:43 +0300

    dev

---
+++ server/srv/alina_consumers/stage/cfg/db.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'xxx',
+    'username'  => 'root',
+    'password'  => 'xxx',
+    'charset'   => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix'    => '',
+    'port'      => 3306,
+];
