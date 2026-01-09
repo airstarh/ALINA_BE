aa6fda0f0b7c3ce26fdad4681692bbc9e427b590
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-01-09 12:25:56 +0300

    dev

--- server/var/www/stage/index.php
+++ server/var/www/stage/index.php
@@ -1,4 +1,5 @@
 <?php
+// STAGE
 define('ALINA_MICROTIME', $_SERVER['REQUEST_TIME_FLOAT'] ?: microtime(TRUE));
 define('ALINA_TIME', $_SERVER['REQUEST_TIME'] ?: time());
 define('ALINA_COOKIE_PAST', ALINA_TIME - 60 * 60);
