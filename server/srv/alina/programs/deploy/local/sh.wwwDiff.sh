eac644b5316640a852ffbe137dc699627189e360
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-01-09 11:38:22 +0300

    dev

--- server/srv/alina/programs/deploy/local/sh.wwwDiff.sh
+++ server/srv/alina/programs/deploy/local/sh.wwwDiff.sh
@@ -63,6 +63,6 @@ BASE="zero.home"
 SOURCE_BASE="/home/qqq/a/b/server/srv/alina_consumers/${BASE}/.WwwDiff/"
 
 PROJECT="stage"
-SOURCE_DIFF="/home/qqq/a/b/server/src/alina_consumers/${PROJECT}/.WwwDiff/"
+SOURCE_DIFF="/home/qqq/a/b/server/srv/alina_consumers/${PROJECT}/.WwwDiff/"
 TARGET="/home/qqq/a/b/server/var/www/${PROJECT}/"
 sync_with_protection "$SOURCE_BASE" "$SOURCE_DIFF" "$TARGET"
