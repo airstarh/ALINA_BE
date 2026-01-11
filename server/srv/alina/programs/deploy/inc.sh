4aa8bf765400a32df3653ae3deab444f378abb02
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-01-11 22:27:31 +0300

    dev

--- server/srv/alina/programs/deploy/inc.sh
+++ server/srv/alina/programs/deploy/inc.sh
@@ -2,18 +2,18 @@
 
 REMOTE_USER="sewa"
 REMOTE_HOST="saysimsim.ru"
-REMOTE_ADDR="${REMOTE_USER}@${REMOTE_HOST}"
+export REMOTE_ADDR="${REMOTE_USER}@${REMOTE_HOST}"
 
-rsyncSsh() {
+export rsyncSsh() {
         local SOURCE="$1"
         local TARGET="$2"
 
-        ssh "${REMOTE_ADDR}" "sudo mkdir -p -m 775 ${TARGET}"
+        ssh "${REMOTE_ADDR}" "sudo mkdir -p -m 755 ${TARGET}"
 
         rsync \
-                -rltDv \
+                --dry-run \
+                -rltv \
                 --zstd \
-                --compress-level=3 \
                 --skip-compress=jpg/jpeg/png/gif/mp4/mp3/zip/gz/pdf \
                 --delete-after \
                 --filter='- **/cfg/db.php' \
