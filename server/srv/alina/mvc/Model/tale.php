a01939235da19888b065afbf1b36ad5c3a94cb66
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-01-31 01:20:10 +0300

    dev

--- server/srv/alina/mvc/Model/tale.php
+++ server/srv/alina/mvc/Model/tale.php
@@ -9,115 +9,129 @@ use Illuminate\Database\Query\Builder as BuilderAlias;
 class tale extends _BaseAlinaModel
 {
     public $flagAuditInfoLog = false;
-    public $table          = 'tale';
-    public $ownerId_root   = NULL;
+    public $table = 'tale';
+    public $ownerId_root = NULL;
     public $ownerId_answer = NULL;
-    public $sortDefault    = [['publish_at', 'DESC']];
+    public $sortDefault = [['publish_at', 'DESC']];
 
     public function fields()
     {
         $pFields = parent::fields();
-        $fields  = [
-            'id'                       => [],
-            'owner_id'                 => [
+        $fields = [
+            'id' => [],
+            'owner_id' => [
                 'default' => CurrentUser::obj()->id(),
             ],
-            'header'                   => [
+            'header' => [
                 'filters' => [
                     ['\alina\utils\Data', 'filterVarStripTags'],
                 ],
             ],
-            'body'                     => [
+            'body' => [
                 'filters' => [
                     ['\alina\utils\Data', 'filterVarStrHtml'],
                 ],
+                'validators' => [
+                    [
+                        // 'f' - Could be a closure, string with function name or an array
+                        'f' =>
+                            function ($v) {
+                                if ($this->attributes->is_submitted == 1) {
+                                    return trim(strip_tags($v));
+                                }
+                                return true;
+                            },
+                        'errorIf' => ['', false, null],
+                        'msg' => ___('Tale Body is required!'),
+                    ],
+                ],
             ],
-            'body_txt'                 => [
+            'body_txt' => [
                 'filters' => [
                     ['\alina\utils\Data', 'filterVarStripTags'],
                 ],
             ],
-            'body_free'                => [
+            'body_free' => [
             ],
-            'iframe'                   => [
+            'iframe' => [
                 'default' => NULL,
             ],
-            'created_at'               => [
+            'created_at' => [
                 'default' => ALINA_TIME,
             ],
-            'modified_at'              => [
+            'modified_at' => [
                 'default' => ALINA_TIME,
             ],
-            'publish_at'               => [
+            'publish_at' => [
                 'default' => ALINA_TIME,
             ],
-            'is_submitted'             => [
+            'is_submitted' => [
                 'default' => 0,
             ],
-            'root_tale_id'             => [
+            'root_tale_id' => [
                 'default' => NULL,
             ],
-            'answer_to_tale_id'        => [
+            'answer_to_tale_id' => [
                 'default' => NULL,
             ],
-            'type'                     => [
+            'type' => [
                 'default' => 'POST',
             ],
-            'level'                    => [
+            'level' => [
                 'default' => 0,
             ],
-            'is_adult_denied'          => [
+            'is_adult_denied' => [
                 'default' => 0,
             ],
-            'is_draft'                 => [
+            'is_draft' => [
                 'default' => 0,
             ],
-            'is_adv'                   => [
+            'is_adv' => [
                 'default' => 0,
             ],
-            'is_comment_denied'        => [
+            'is_comment_denied' => [
                 'default' => 0,
             ],
-            'is_sticked'               => [
+            'is_sticked' => [
                 'default' => 0,
             ],
-            'is_sticked_on_home'       => [ //ToDo: Seems not in use
+            'is_sticked_on_home' => [ //ToDo: Seems not in use
                 'default' => 0,
             ],
-            'is_header_hidden'         => [
+            'is_header_hidden' => [
                 'default' => 0,
             ],
-            'is_date_hidden'           => [
+            'is_date_hidden' => [
                 'default' => 0,
             ],
-            'is_avatar_hidden'         => [
+            'is_avatar_hidden' => [
                 'default' => 0,
             ],
             'is_social_sharing_hidden' => [
                 'default' => 0,
             ],
-            'is_for_registered'        => [
+            'is_for_registered' => [
                 'default' => 0,
             ],
-            'is_comment_for_owner'     => [
+            'is_comment_for_owner' => [
                 'default' => 0,
             ],
-            'seo_index'                => [
+            'seo_index' => [
                 'default' => 0.2,
             ],
-            'geo_latitude'             => [
+            'geo_latitude' => [
                 'default' => 0,
             ],
-            'geo_longitude'            => [
+            'geo_longitude' => [
                 'default' => 0,
             ],
-            'geo_map_type'             => [
+            'geo_map_type' => [
                 'default' => 'hybrid',
             ],
-            'geo_zoom'                 => [
+            'geo_zoom' => [
                 'default' => 10,
             ],
-            'geo_is_map_shown'         => [
+            'geo_is_map_shown' => [
                 'default' => 0,
             ],
         ];
@@ -129,40 +143,48 @@ class tale extends _BaseAlinaModel
     public function referencesTo()
     {
         return [
-            'owner'        => [
-                'has'        => 'one',
-                'joins'      => [
+            'owner' => [
+                'has' => 'one',
+                'joins' => [
                     ['leftJoin', 'user AS owner', 'owner.id', '=', "{$this->alias}.owner_id"],
                 ],
                 'conditions' => [],
                 'addSelects' => [
-                    ['addSelect', [
-                        'owner.id AS owner_id',
-                        'owner.firstname AS owner_firstname',
-                        'owner.lastname AS owner_lastname',
-                        'owner.emblem AS owner_emblem',
-                    ]],
+                    [
+                        'addSelect',
+                        [
+                            'owner.id AS owner_id',
+                            'owner.firstname AS owner_firstname',
+                            'owner.lastname AS owner_lastname',
+                            'owner.emblem AS owner_emblem',
+                        ]
+                    ],
                 ],
             ],
             'router_alias' => [
-                'has'        => 'one',
-                'joins'      => [
+                'has' => 'one',
+                'joins' => [
                     //['leftJoin', 'router_alias AS rs', 'rs.table_id', '=', "{$this->alias}.id"],
-                    ['leftJoin', 'router_alias AS rs', function ($join) {
-                        $join->on('rs.table_id', '=', "{$this->alias}.id");
-                        $join->on('rs.table', '=', Dal::raw("'tale'"));
-                    }],
+                    [
+                        'leftJoin',
+                        'router_alias AS rs',
+                        function ($join) {
+                            $join->on('rs.table_id', '=', "{$this->alias}.id");
+                            $join->on('rs.table', '=', Dal::raw("'tale'"));
+                        }
+                    ],
                 ],
                 'conditions' => [],
                 'addSelects' => [
-                    ['addSelect',
+                    [
+                        'addSelect',
                         ['rs.alias AS router_alias', 'rs.id AS router_alias_id'],
                     ],
                 ],
             ],
-            'tag'          => [
-                'has'        => 'manyThrough',
-                'joins'      => [
+            'tag' => [
+                'has' => 'manyThrough',
+                'joins' => [
                     ['leftJoin', 'tag_to_entity AS glue', 'glue.entity_id', '=', "{$this->alias}.{$this->pkName}"],
                     ['leftJoin', 'tag AS child', 'child.id', '=', 'glue.tag_id'],
                 ],
@@ -172,14 +194,14 @@ class tale extends _BaseAlinaModel
                 'addSelects' => [
                     ['addSelect', ['child.name AS tag_name', 'child.id AS child_id', 'glue.id AS ref_id', "{$this->alias}.{$this->pkName} AS main_id"]],
                 ],
-                'orders'     => [
+                'orders' => [
                     ['orderBy', 'child.name', 'ASC'],
                 ],
             ],
-            'body'         => [
+            'body' => [
                 'type' => 'textarea',
             ],
-            'body_txt'     => [
+            'body_txt' => [
                 'type' => 'readonly',
             ],
         ];
@@ -230,45 +252,45 @@ class tale extends _BaseAlinaModel
         }
         #####
         #region Double check parents
-        $root_tale_id      = NULL;
+        $root_tale_id = NULL;
         $answer_to_tale_id = NULL;
-        $level             = 0;
-        $type              = 'POST';
+        $level = 0;
+        $type = 'POST';
         if (array_key_exists('answer_to_tale_id', $dataArray)) {
             #####
             # ANSWER p1 - parent-1
             if (!empty($dataArray['answer_to_tale_id'])) {
-                $p1_id   = $dataArray['answer_to_tale_id'];
-                $p1      = new tale();
-                $p1Q     = $p1->q();
+                $p1_id = $dataArray['answer_to_tale_id'];
+                $p1 = new tale();
+                $p1Q = $p1->q();
                 $p1Attrs = $p1Q->select(['id', 'root_tale_id', 'answer_to_tale_id', 'owner_id'])->where([['id', $p1_id]])->first();
                 #####
                 # ROOT p2 - parent-2
                 if (isset($p1Attrs->id) && !empty($p1Attrs->id)) {
-                    $root_tale_id         = $p1Attrs->id;
-                    $answer_to_tale_id    = $p1Attrs->id;
-                    $level                = 1;
-                    $type                 = 'COMMENT';
+                    $root_tale_id = $p1Attrs->id;
+                    $answer_to_tale_id = $p1Attrs->id;
+                    $level = 1;
+                    $type = 'COMMENT';
                     $this->ownerId_answer = $p1Attrs->owner_id;
                     if (isset($p1Attrs->answer_to_tale_id) && !empty($p1Attrs->answer_to_tale_id)) {
-                        $p2_id   = $p1Attrs->answer_to_tale_id;
-                        $p2      = new tale();
-                        $p2Q     = $p2->q();
+                        $p2_id = $p1Attrs->answer_to_tale_id;
+                        $p2 = new tale();
+                        $p2Q = $p2->q();
                         $p2Attrs = $p2Q->select(['id', 'root_tale_id', 'answer_to_tale_id', 'owner_id'])->where([['id', $p2_id]])->first();
                         if (isset($p2Attrs->id) && !empty($p2Attrs->id)) {
-                            $root_tale_id       = $p2Attrs->id;
-                            $level              = 2;
-                            $type               = 'COMMENT';
+                            $root_tale_id = $p2Attrs->id;
+                            $level = 2;
+                            $type = 'COMMENT';
                             $this->ownerId_root = $p2Attrs->owner_id;
                         }
                     }
                 }
             }
         }
-        $dataArray['root_tale_id']      = $root_tale_id;
+        $dataArray['root_tale_id'] = $root_tale_id;
         $dataArray['answer_to_tale_id'] = $answer_to_tale_id;
-        $dataArray['level']             = $level;
-        $dataArray['type']              = $type;
+        $dataArray['level'] = $level;
+        $dataArray['type'] = $type;
         #endregion Double check parents
         #####
         #####
