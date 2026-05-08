caebf24aa82849152a8aee19a4fc767f6f6be805
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-05-08 17:37:18 +0300

    dev

--- server/srv/alina/programs/deploy/db-restore.sh
+++ server/srv/alina/programs/deploy/db-restore.sh
@@ -4,6 +4,8 @@ PATH_SETUP_EXE="$(readlink -f "$0")"
 DIR_SETUP_EXE="$(dirname "${PATH_SETUP_EXE}")"
 source "${DIR_SETUP_EXE}/inc.sh"
 
+DUMP_FILE_NAME="db_m45a.sql"
+
 echo ""
 echo "Starting import..."
 
@@ -17,7 +19,7 @@ docker exec alina_mysql mysql \
   --max_allowed_packet=2G \
   --net_buffer_length=1M \
   -e "SET FOREIGN_KEY_CHECKS=0; SET UNIQUE_CHECKS=0; SET AUTOCOMMIT=0;" \
-  -e "SOURCE ${ALINA_LOCAL_DB_DUMPS}/db_m45a.sql;" \
+  -e "SOURCE ${ALINA_LOCAL_DB_DUMPS}/${DUMP_FILE_NAME};" \
   -e "COMMIT; SET FOREIGN_KEY_CHECKS=1; SET UNIQUE_CHECKS=1;"
 
 END_TIME=$(date +%s)
