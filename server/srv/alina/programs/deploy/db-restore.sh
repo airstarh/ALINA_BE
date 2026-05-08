874523e94bea6729413ce0f4e0799fe6870fee05
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-05-08 16:06:16 +0300

    dev

--- server/srv/alina/programs/deploy/db-restore.sh
+++ server/srv/alina/programs/deploy/db-restore.sh
@@ -2,18 +2,15 @@
 
 PATH_SETUP_EXE="$(readlink -f "$0")"
 DIR_SETUP_EXE="$(dirname "${PATH_SETUP_EXE}")"
-
 source "${DIR_SETUP_EXE}/inc.sh"
 
-#####
-
-# docker exec alina_mysql ls -la ${ALINA_LOCAL_DB_DUMPS}/db_m45a.sql
-
-# docker exec alina_mysql mysql -u "${ALINA_LOCAL_DB_USER}" -p"${ALINA_LOCAL_DB_PASS}" -e "SOURCE ${ALINA_LOCAL_DB_DUMPS}/db_m45a.sql"
-
-# Combine everything into a single command with -e
-docker exec alina_mysql mysql -u "${ALINA_LOCAL_DB_USER}" -p"${ALINA_LOCAL_DB_PASS}" \
+docker exec alina_mysql mysql \
+  -u "${ALINA_LOCAL_DB_USER}" \
+  -p"${ALINA_LOCAL_DB_PASS}" \
+  --batch \
+  --quick \
+  --max_allowed_packet=3G \
+  --net_buffer_length=1M \
   -e "SET FOREIGN_KEY_CHECKS=0; SET UNIQUE_CHECKS=0; SET AUTOCOMMIT=0;" \
   -e "SOURCE ${ALINA_LOCAL_DB_DUMPS}/db_m45a.sql;" \
   -e "COMMIT; SET FOREIGN_KEY_CHECKS=1; SET UNIQUE_CHECKS=1;"
-
