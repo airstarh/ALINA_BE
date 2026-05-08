1c33125aef71a48e1579d192fd3503c4b763a33f
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-05-08 15:05:27 +0300

    dev

--- server/srv/alina/programs/deploy/db-restore.sh
+++ server/srv/alina/programs/deploy/db-restore.sh
@@ -7,11 +7,15 @@ source "${DIR_SETUP_EXE}/inc.sh"
 
 #####
 
-docker exec alina_mysql mysql -u "${ALINA_DB_USER}" -p"${ALINA_DB_PASS}" <<EOSQL
+docker exec alina_mysql ls -la ${ALINA_LOCAL_DB_DUMPS}/db_m45a.sql
+
+# docker exec alina_mysql mysql -u "${ALINA_LOCAL_DB_USER}" -p"${ALINA_LOCAL_DB_PASS}" -e "SOURCE ${ALINA_LOCAL_DB_DUMPS}/db_m45a.sql"
+
+docker exec alina_mysql mysql -v -u "${ALINA_LOCAL_DB_USER}" -p"${ALINA_LOCAL_DB_PASS}" <<EOSQL
 SET FOREIGN_KEY_CHECKS=0;
 SET UNIQUE_CHECKS=0;
 SET AUTOCOMMIT=0;
-SOURCE ${ALINA_DB_DUMPS_DIR}/db_m45a.sql;
+SOURCE ${ALINA_LOCAL_DB_DUMPS}/db_m45a.sql;
 COMMIT;
 SET FOREIGN_KEY_CHECKS=1;
 SET UNIQUE_CHECKS=1;
