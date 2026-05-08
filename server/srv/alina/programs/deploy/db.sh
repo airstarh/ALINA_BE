74704a940a80dc1b1d842e8fed57e919c71b2e0e
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-05-08 10:55:02 +0300

    dev

---
+++ server/srv/alina/programs/deploy/db.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PATH_SETUP_EXE="$(readlink -f "$0")"
+DIR_SETUP_EXE="$(dirname "${PATH_SETUP_EXE}")"
+
+source "${DIR_SETUP_EXE}/inc.sh"
+
+#####
+
+mkdir -p "${ALINA_REMOTE_DUMP_DIR}"
+
+# Dump on remote and copy to local
+for db in "${ALINA_DB_BASES[@]}"; do
+    ssh "${REMOTE_ADDR}" \
+        "mysqldump -u "${ALINA_DB_USER}" -p"${ALINA_DB_PASS}" \
+        --databases '$db' \
+        --add-drop-database \
+        --replace \
+        --single-transaction \
+        --routines \
+        --triggers \
+        --events" > "${ALINA_REMOTE_DUMP_DIR}/db_${db}.sql"
+
+    echo "Dumped: $db"
+done
