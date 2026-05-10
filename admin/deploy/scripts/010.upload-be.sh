201b30cb476b9c80ec037064391e1cc51c952da6
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-05-10 10:35:22 +0300

    dev

--- admin/deploy/scripts/010.upload-be.sh
+++ admin/deploy/scripts/010.upload-be.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-REMOTE_TARGET="/srv"
+REMOTE_BASE_FOLDER="/srv"
 
 # Define array of projects
-PROJECTS=(
+ALINA_BE_FOLDERS=(
     "alina"
     "alina_consumers"
 )
 
 # Loop through each project
-for PROJECT in "${PROJECTS[@]}"; do
+for BE_FOLDER in "${ALINA_BE_FOLDERS[@]}"; do
     echo ""
-    echo ">>> ${PROJECT}"
+    echo ">>> ${BE_FOLDER}"
 
-    SOURCE="${BE}/server/srv/${PROJECT}/"
-    TARGET="${REMOTE_TARGET}/${PROJECT}/"
+    SOURCE_ALINA_BE_FOLDER="${BE}/server/srv/${BE_FOLDER}/"
+    TARGET_ALINA_BE_FOLDER="${REMOTE_BASE_FOLDER}/${BE_FOLDER}/"
 
-    rsyncSsh "${SOURCE}" "${TARGET}"
+    rsyncSsh "${SOURCE_ALINA_BE_FOLDER}" "${TARGET_ALINA_BE_FOLDER}"
 
-    echo "<<< ${PROJECT}"
+    echo "<<< ${BE_FOLDER}"
     echo ""
 done
 
