a5063c4fd785ba701973466f25002abc9c0234e8
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-02 19:49:45 +0300

    dev

---
+++ server/srv/sewa/cert003/cert/080.trust.android.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+#
+# Ensure you have your certificate in the right format. Android supports .crt, .p12, .cer, and .pfx files
+#
+openssl x509 -in 040.byrobot.fullchain.pem -outform DER -out 090.zero-home.crt
