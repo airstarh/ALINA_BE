bf8a9b9e8ed3cf9bdd661e2d01df456e5547faff
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-02 19:50:44 +0300

    dev

--- server/srv/sewa/cert003/cert/080.trust.android.sh
+++ server/srv/sewa/cert003/cert/080.trust.android.sh
@@ -4,3 +4,8 @@
 # Ensure you have your certificate in the right format. Android supports .crt, .p12, .cer, and .pfx files
 #
 openssl x509 -in 040.byrobot.fullchain.pem -outform DER -out 090.zero-home.crt
+
+#
+# Transfer file to device and find a way to import
+# 090.zero-home.crt
+#
