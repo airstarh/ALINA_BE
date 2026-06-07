1be26ef612548cdff2ee64254ee0513127c0aedc
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-07 04:14:45 +0300

    var

--- dc.cert.sh
+++ dc.cert.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 
-docke compose \
--f dc.yml \
-run --rm \
-alina_certbot \
-certonly \
---webroot \
---webroot-path=/var/www/certbot \
---email vsevolod.azovsky@gmail.com \
---agree-tes--no-eff-email \
--d saysimsim.ru \
--d mironova45a.ru \
--d ospl1942.ru \
--d stage.saysimsim.ru \
--d chat.saysimsim.ru
+docker compose \
+  -f dc.yml \
+  run --rm \
+  alina_certbot \
+  certonly \
+  --webroot \
+  --webroot-path=/var/www/certbot \
+  --email vsevolod.azovsky@gmail.com \
+  --agree-tos \
+  --no-eff-email \
+  -d saysimsim.ru \
+  -d mironova45a.ru \
+  -d ospl1942.ru \
+  -d stage.saysimsim.ru \
+  -d chat.saysimsim.ru
