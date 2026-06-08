b66f5fb03987facf7d4789feb2723d93822dca97
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-08 12:48:29 +0300

    dev

--- adm/db/restore/index.sh
+++ adm/db/restore/index.sh
@@ -2,5 +2,6 @@
 
 source ./adm/s.includes.sh
 
-zcat ./_GITOUT/db/stage.sql.gz | docker exec -i alina_mysql mysql -u root -p"${MYSQL_ROOT_PASSWORD}"
+LOC_DB="stage"
+zcat ./_GITOUT/db/${LOC_DB}.sql.gz | docker exec -i alina_mysql mysql -u root -p"${MYSQL_ROOT_PASSWORD}"
 
