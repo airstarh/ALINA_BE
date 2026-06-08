6b1a09e25105dbbd43287a3b27200a6a7f870365
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-08 06:49:32 +0300

    dev

--- adm/s.constants.sh
+++ adm/s.constants.sh
@@ -7,12 +7,7 @@ export ALINA_REMOTE_USER="sewa"
 export ALINA_REMOTE_URL="${ALINA_REMOTE_HOST}@${ALINA_REMOTE_USER}"
 # endregion SERVER
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# region XXX
-
-ALINA_ROOT=$(pwd)
-export ALINA_ROOT
+# region Databes
 
 export ALINA_BASES=(
     "alina" # sss
@@ -20,17 +15,34 @@ export ALINA_BASES=(
     "vov"
     "m45a"
 )
+# endregion Databes
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# region CODE
+export ALINA_BE_LOCAL_DIR="/home/qqq/_A001/rep/ALINA_BE"
+export ALINA_FE_LOCAL_DIR="/home/qqq/_A001/rep/ALINA_FE"
 
-export ALINA_FOLDERS=(
+export ALINA_DOCKER_VOL_BE="server/srv"
+export ALINA_DOCKER_VOL_FE="server/var/www"
+
+export ALINA_BE_REMOTE_DIR="/srv"
+export ALINA_FE_REMOTE_DIR="/var/www"
+
+export ALINA_FRAMEWORK="alina"
+export ALINA_FRAMEWORK_CONSUMERS="alina_consumers"
+export ALINA_BE_FREAMEWORK_FOLDERS=(
+    "${ALINA_FRAMEWORK}"
+    "${ALINA_FRAMEWORK_CONSUMERS}"
+)
+
+export ALINA_PROJECTS=(
+    "zero.home"
+    "stage"
     "saysimsim.ru"
-    "zero.home"     # uses DB stage
-    "stage"         # uses DB stage
     "m45a"
     "vov"
-    # "chat"
 )
-# endregion XXX
-
+export ALINA_DEFAULT_PROJECT="zero.home"
+# endregion CODE
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # region XXX
 echo ''
--- admin/constants.sh
+++ admin/constants.sh
@@ -8,34 +8,6 @@ export REMOTE_HOST="saysimsim.ru"
 export REMOTE_ADDR="${REMOTE_USER}@${REMOTE_HOST}"
 
 ####################################################################################################
-
-# Code
-export ALINA_BE_LOCAL_DIR="/home/qqq/_A001/rep/ALINA_BE"
-export ALINA_FE_LOCAL_DIR="/home/qqq/_A001/rep/ALINA_FE"
-
-export ALINA_DOCKER_VOL_BE="server/srv"
-export ALINA_DOCKER_VOL_FE="server/var/www"
-
-export ALINA_BE_REMOTE_DIR="/srv"
-export ALINA_FE_REMOTE_DIR="/var/www"
-
-export ALINA_FRAMEWORK="alina"
-export ALINA_FRAMEWORK_CONSUMERS="alina_consumers"
-export ALINA_BE_FREAMEWORK_FOLDERS=(
-    "${ALINA_FRAMEWORK}"
-    "${ALINA_FRAMEWORK_CONSUMERS}"
-)
-
-export ALINA_PROJECTS=(
-    "zero.home"
-    "stage"
-    "saysimsim.ru"
-    "m45a"
-    "vov"
-)
-export ALINA_DEFAULT_PROJECT="zero.home"
-
-####################################################################################################
 # Databsase
 export ALINA_DB_USER="root"
 export ALINA_DB_PASS="1378862"
