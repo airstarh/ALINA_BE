d9a1498c1590884d82a4af260f872720ed502de9
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-08 15:42:12 +0300

    dev

--- a.dev.perms.sh
+++ a.dev.perms.sh
@@ -20,7 +20,7 @@ sudo chown -R "${ALINA_USER_PHP}":"${ALINA_GROUP_PHP}" ./server/var/log/php
 sudo chown -R "${ALINA_USER_PHP}":"${ALINA_GROUP_PHP}" ./server/var/www
 sudo chown -R "${ALINA_USER_PHP}":"${ALINA_GROUP_PHP}" ./server/srv/alina
 sudo chown -R "${ALINA_USER_PHP}":"${ALINA_GROUP_PHP}" ./server/srv/alina_consumers
-sudo chmod "${ALINA_DIR}" ./server/var/log/php
+sudo chmod -R "${ALINA_DIR}" ./server/var/log/php
 sudo find ./server/var/www -type d -exec chmod "${ALINA_DIR}" {} \;
 sudo find ./server/var/www -type f -exec chmod "${ALINA_FILE}" {} \;
 sudo find ./server/srv/alina -type d -exec chmod "${ALINA_DIR}" {} \;
@@ -30,10 +30,14 @@ sudo find ./server/srv/alina_consumers -type f -exec chmod "${ALINA_FILE}" {} \;
 
 # NGINX (nginx 101)
 sudo chown -R "${ALINA_USER_NGINX}":"${ALINA_GROUP_NGINX}" ./server/var/log/nginx
+sudo chown -R "${ALINA_USER_NGINX}":"${ALINA_GROUP_NGINX}" ./server/etc/nginx
+sudo chown -R "${ALINA_USER_NGINX}":"${ALINA_GROUP_NGINX}" ./server/etc/letsencrypt
 sudo chown "${ALINA_USER_NGINX}":"${ALINA_GROUP_NGINX}" ./server/srv/sewa/cert003/cert/*.pem
 sudo chmod "${ALINA_FILE}" ./server/srv/sewa/cert003/cert/*.pem
-sudo chmod "${ALINA_DIR}" ./server/srv/sewa/cert003/cert
-sudo chmod "${ALINA_DIR}" ./server/var/log/nginx
+sudo chmod -R "${ALINA_DIR}" ./server/srv/sewa/cert003/cert
+sudo chmod -R "${ALINA_DIR}" ./server/var/log/nginx
+sudo chmod -R "${ALINA_DIR}" ./server/etc/nginx
+sudo chmod -R "${ALINA_DIR}" ./server/etc/letsencrypt
 
 # CONFIGS PROTECTED SECURITY
 sudo chmod "${ALINA_FILE}" ./server/etc/nginx/*.conf
