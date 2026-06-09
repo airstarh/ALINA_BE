6fa76342091e1baf0e1594040f0a04f70df90efa
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-09 05:01:51 +0300

    dev

--- a.prod.cert.sh
+++ a.prod.cert.sh
@@ -12,9 +12,16 @@ docker compose \
   --agree-tos \
   --no-eff-email \
   -d saysimsim.ru \
+  -d www.saysimsim.ru \
   -d mironova45a.ru \
+  -d www.mironova45a.ru \
   -d ospl1942.ru \
+  -d www.ospl1942.ru \
   -d stage.saysimsim.ru \
+  -d chat.saysimsim.ru \
+  --cert-name saysimsim.ru \
+  --force-renewal
+
 
 # bash ./a.prod.perms.sh
 # docker compose -f dc.yml -f dc.prod.yml exec nginx nginx -s reload
