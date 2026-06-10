36b01393c9a18c82afc9d9067b35e2cef66ed051
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-10 10:07:25 +0300

    dev

--- adm/code/030.alina.to.remote.sh
+++ adm/code/030.alina.to.remote.sh
@@ -1,18 +1,11 @@
-a#!/bin/bash
+#!/bin/bash
 
 
 for LOC_PROJECT in "${A_LIST_CORE[@]}"; do
-
-    echo ""
-    echo ">>> ${LOC_PROJECT}"
 
     LOC_SOURCE="${A_L_BE}/${A_L_SRV}/${LOC_PROJECT}/"
     LOC_TARGET="${A_R_SRV}/${LOC_PROJECT}/"
 
     alina_rsync_ssh "${LOC_SOURCE}" "${LOC_TARGET}"
 
-    echo "<<< ${LOC_PROJECT}"
-    echo ""
 done
-
-echo "✅ ✅ ✅ ✅"
