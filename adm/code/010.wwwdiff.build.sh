f19f9ac4eb378edee9e40cc59ecd31d9505f7f27
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-10 10:05:53 +0300

    dev

--- adm/code/010.wwwdiff.build.sh
+++ adm/code/010.wwwdiff.build.sh
@@ -2,10 +2,10 @@
 
 LOC_DIFF_DEFAULT="${A_L_BE}/${A_L_SRV}/${A_CONSUMERS}/${ALINA_DEFAULT_PROJECT}/.WwwDiff/"
 
-for PROJECT in "${A_LIST_PROJECTS[@]}"; do
+for LOC_PROJECT in "${A_LIST_PROJECTS[@]}"; do
 
-    LOC_DIFF_SOURCE="${A_L_BE}/${A_L_SRV}/${A_CONSUMERS}/${PROJECT}/.WwwDiff/"
-    LCO_TARGET="${A_L_BE}/${A_L_VAR_WWW}/${PROJECT}/"
+    LOC_DIFF_SOURCE="${A_L_BE}/${A_L_SRV}/${A_CONSUMERS}/${LOC_PROJECT}/.WwwDiff/"
+    LCO_TARGET="${A_L_BE}/${A_L_VAR_WWW}/${LOC_PROJECT}/"
 
     alina_rsync_local "${LOC_DIFF_DEFAULT}" "${LOC_DIFF_SOURCE}" "${LCO_TARGET}"
 
--- adm/code/020.www.to.remote.sh
+++ adm/code/020.www.to.remote.sh
@@ -2,16 +2,9 @@
 
 for LOC_PROJECT in "${A_LIST_PROJECTS[@]}"; do
 
-    echo ""
-    echo ">>> ${LOC_PROJECT}"
-
     LOC_SOURCE="${A_L_BE}/${A_L_VAR_WWW}/${LOC_PROJECT}/"
     LOC_TARGET="${A_R_VAR_WWW}/${LOC_PROJECT}/"
 
     alina_rsync_ssh "${LOC_SOURCE}" "${LOC_TARGET}"
 
-    echo "<<< ${LOC_PROJECT}"
-    echo ""
 done
-
-echo "✅ ✅ ✅ ✅"
