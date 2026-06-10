3a0ab3ef99afc349ac24d5f07631012687b9c666
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-10 09:09:18 +0300

    dev

--- adm/code.old/upload.sh
+++ adm/code.old/upload.sh
@@ -15,9 +15,6 @@ cd "${ALINA_FE_LOCAL_DIR}"
 cd "${DIR_THIS}" || { echo "Failed to cd to ${DIR_THIS}"; exit 1; }
 ####################################################################################################
 # OWN
-# source ./adm/code.old/scripts/000.ownership.sh
 # source ./adm/code.old/scripts/010.upload-be.sh
 source ./adm/code.old/scripts/015.www-diff.sh
 # source ./adm/code.old/scripts/020.upload-web.sh
-# source ./adm/code.old/scripts/000.ownership.sh
-# source ./adm/code.old/scripts/030.restart.sh
