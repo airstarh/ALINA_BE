cebc14967ef0c2f6ccb326db9941ecf20616b32c
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-11 11:57:11 +0300

    dev

--- adm/code/a.to.local.vue2.sh
+++ adm/code/a.to.local.vue2.sh
@@ -1,20 +1,12 @@
-#!/bin/bash
+#! /bin/bash
 
 source ./adm/s.includes.sh
 
-cd "${A_L_FE}"
+cd "${A_L_FE}" || { echo "Failed to cd to ${A_L_FE}"; exit 1; }
 ####################################################################################################
 # FRONT
 # nvm install "16.19.1"
 # nvm use "16.19.1"
-# npm run build:all
-
-
-
-
-cd "${A_L_BE}" || { echo "Failed to cd to ${DIR_THIS}"; exit 1; }
+npm run build:all
 ####################################################################################################
-# OWN
-source ./adm/bin/script/code/010.wwwdiff.build.sh
-# source ./adm/bin/script/code/020.www.to.remote.sh
-# source ./adm/bin/script/code/030.alina.to.remote.sh
+cd "${A_L_BE}" || { echo "Failed to cd to ${A_L_BE}"; exit 1; }
---
+++ adm/code/a.to.prod.server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source ./adm/s.includes.sh
+
+source ./adm/code/a.to.local.vue2.sh
+
+source ./adm/code/a.to.local.wwwdiff.sh
+
+source ./adm/bin/script/code/020.www.to.remote.sh
+
+source ./adm/bin/script/code/030.alina.to.remote.sh
