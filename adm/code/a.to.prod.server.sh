5d994be91ff959ab36b7e25e19e0cb940d323df9
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-11 14:21:59 +0300

    dev

--- adm/code/a.to.prod.server.sh
+++ adm/code/a.to.prod.server.sh
@@ -2,10 +2,8 @@
 
 source ./adm/s.includes.sh
 
-source ./adm/code/a.to.local.vue2.sh
-
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 source ./adm/code/a.to.local.wwwdiff.sh
-
+source ./adm/code/a.to.local.vue2.sh
 source ./adm/bin/script/code/020.www.to.remote.sh
-
 source ./adm/bin/script/code/030.alina.to.remote.sh
