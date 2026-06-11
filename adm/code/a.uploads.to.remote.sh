85ec42a644805cf0232ac728b782485581f35ef6
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-11 14:10:43 +0300

    dev

--- adm/code/a.uploads.to.remote.sh
+++ adm/code/a.uploads.to.remote.sh
@@ -1,1 +1,12 @@
+#! /bin/bash
 
+source ./adm/s.includes.sh
+
+for LOC_PROJECT in "${A_LIST_PROJECTS[@]}"; do
+
+    LOC_SOURCE="${A_L_BE}/${A_L_VAR_WWW}/${LOC_PROJECT}/uploads"
+    LOC_TARGET="${A_R_BE}/${A_R_VAR_WWW}/${LOC_PROJECT}/uploads"
+
+    alina_rsync_to_remote "${LOC_SOURCE}" "${LOC_TARGET}"
+
+done
