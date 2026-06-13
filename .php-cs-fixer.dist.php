096070b94360485079ae5d429220a6ff8e46eec7
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-13 11:08:01 +0300

    dev SUCCESS PHP CS FIXER

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,13 +1,13 @@
 <?php
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__ . '/server/var/www') // ваш код в /var/www
+    ->in(__DIR__ . '/server/srv/alina')
     ->name('*.php')
     ->notName('*.blade.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR12' => true,
         'array_syntax' => ['syntax' => 'short'],
