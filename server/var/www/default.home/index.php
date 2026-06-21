bc2aecfb81deb95e66685261f6847adbffaf20ab
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-21 23:22:59 +0300

    dev

--- server/var/www/default.home/index.php
+++ server/var/www/default.home/index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo 'Default site';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+####################################################################################################
+
+$res = $aasdasd->prop->asd ?? 'sewa';
+// $res = 2/0;
+
+####################################################################################################
+echo PHP_EOL;
+echo PHP_EOL;
+var_export($res, 0);
+echo PHP_EOL;
+echo PHP_EOL;
+echo PHP_EOL;
