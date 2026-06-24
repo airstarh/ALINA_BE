9f9b43e65c1c5d979f75fe2c06ff88ca18e82a36
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-24 22:12:37 +0300

    dev

--- adm/db/migration/old/2026-06-24-tranlations/sql.sql
+++ adm/db/migration/old/2026-06-24-tranlations/sql.sql
@@ -11,3 +11,6 @@ ALTER TABLE voc MODIFY COLUMN `from` VARCHAR(500) CHARACTER SET utf8mb4 COLLATE 
 ALTER TABLE voc MODIFY COLUMN en_US VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL;
 ALTER TABLE voc MODIFY COLUMN ru_RU VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL;
 ALTER TABLE voc ADD CONSTRAINT cns_voc_from UNIQUE KEY (`from`);
+
+ALTER TABLE watch_banned_visit ADD user_id BIGINT NULL;
+CREATE INDEX watch_banned_visit_browser_enc_IDX USING BTREE ON watch_banned_visit (browser_enc,user_id,ip);
