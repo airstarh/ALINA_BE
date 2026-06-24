9f5864154ebf12d1ecd0f5bf22505609edddefeb
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-24 20:24:05 +0300

    dev

--- server/srv/alina/functions/_dependent/alinaFacade.php
+++ server/srv/alina/functions/_dependent/alinaFacade.php
@@ -132,7 +132,7 @@ function AlinaReject($page = null, $code = 403, $message = 'ACCESS DENIED', $mes
  * What is correct HTTP status code when redirecting to a login page?
  * https://stackoverflow.com/questions/2839585/what-is-correct-http-status-code-when-redirecting-to-a-login-page
  */
-function AlinaRejectIfNotLoggedIn($code = 403)
+function AlinaRejectIfNotLoggedIn($code = 302)
 {
     if (! AlinaAccessIfLoggedIn()) {
         AlinaReject(AlinaCfg('frontend/login'), $code);
