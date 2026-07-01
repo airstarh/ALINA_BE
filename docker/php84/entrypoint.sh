990d2e154c1cab978933082539dc243d86d8cb4b
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-07-01 23:12:57 +0300

    dev

--- docker/php84/entrypoint.sh
+++ docker/php84/entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
-set -e
 
-# cd /srv/alina || { echo "ERROR: /srv/alina does not exist"; exit 1; }
-#
-# mkdir -p /srv/alina/vendor
-# mkdir -p storage bootstrap/cache
-# chown -R www-data:www-data storage bootstrap/cache 2>/dev/null || true
-# chown -R www-data:www-data /srv/alina/vendor || true
-#
-# composer install --no-interaction --optimize-autoloader
-
-echo "Entrypoint finished, starting main process..."
+. /srv/entrypoint.vol.php.sh
 exec "$@"
+
