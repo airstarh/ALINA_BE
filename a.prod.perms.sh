30f940d1dbedae1f84a47bcabf3f12dfa64639f3
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-07-01 22:00:40 +0300

    dev

--- a.prod.perms.sh
+++ a.prod.perms.sh
@@ -27,29 +27,30 @@ ALINA_PERMISSIONS() {
     local PERM_DIR="${4:-$ALINA_DIR}"
     local PERM_FIL="${5:-$ALINA_FILE}"
 
-    if [[ ! -d "$P" ]]; then
+    if [[ ! -e "$P" ]]; then
         echo "WARNING: '$P' not found, skipped." >&2
         return 0
     fi
 
     chown -R "$U:$G" "$P"
     find "$P" -type d -exec chmod "$PERM_DIR" {} \;
+    find "$P" -type d -exec chmod g+s {} \;
     find "$P" -type f -exec chmod "$PERM_FIL" {} \;
 }
 
 # === MySQL ===
 chown -R "$ALINA_USER_MYSQL:$ALINA_GROUP_MYSQL" "$BASE_DIR/database/mysql"
-ALINA_PERMISSIONS "$ALINA_USER_MYSQL" "$ALINA_GROUP_MYSQL" "server/var/log/mysql" 755 664
+ALINA_PERMISSIONS "$ALINA_USER_MYSQL" "$ALINA_GROUP_MYSQL" "server/var/log/mysql" 775 664
 
 # === PHP ===
-ALINA_PERMISSIONS "$ALINA_USER_PHP" "$ALINA_GROUP_PHP" "server/var/log/php" 755 664
+ALINA_PERMISSIONS "$ALINA_USER_PHP" "$ALINA_GROUP_PHP" "server/var/log/php" 775 664
 ALINA_PERMISSIONS "$ALINA_USER_PHP" "$ALINA_GROUP_PHP" "server/var/www"
 ALINA_PERMISSIONS "$ALINA_USER_PHP" "$ALINA_GROUP_PHP" "server/srv/alina"
 ALINA_PERMISSIONS "$ALINA_USER_PHP" "$ALINA_GROUP_PHP" "server/srv/alina_consumers"
 
 # === NGINX ===
-ALINA_PERMISSIONS "$ALINA_USER_NGINX" "$ALINA_GROUP_NGINX" "server/var/log/nginx" 755 664
-ALINA_PERMISSIONS "$ALINA_USER_NGINX" "$ALINA_GROUP_NGINX" "server/var/log/letsencrypt" 755 664
+ALINA_PERMISSIONS "$ALINA_USER_NGINX" "$ALINA_GROUP_NGINX" "server/var/log/nginx" 775 664
+ALINA_PERMISSIONS "$ALINA_USER_NGINX" "$ALINA_GROUP_NGINX" "server/var/log/letsencrypt" 775 664
 ALINA_PERMISSIONS "$ALINA_USER_NGINX" "$ALINA_GROUP_NGINX" "server/etc/nginx"
 ALINA_PERMISSIONS "$ALINA_USER_NGINX" "$ALINA_GROUP_NGINX" "server/etc/letsencrypt"
 ALINA_PERMISSIONS "$ALINA_USER_NGINX" "$ALINA_GROUP_NGINX" "server/srv/sewa/cert003"
