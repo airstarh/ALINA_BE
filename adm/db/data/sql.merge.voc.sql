ca08e406a39b2c4cd3e63e8618425609dfd07a6d
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-07-05 00:07:22 +0300

    dev

--- adm/db/data/sql.merge.voc.sql
+++ adm/db/data/sql.merge.voc.sql
@@ -2,6 +2,5 @@ INSERT INTO alina.voc (`from`, en_US, ru_RU)
 SELECT s.`from`, s.en_US, s.ru_RU
 FROM stage.voc AS s
 ON DUPLICATE KEY UPDATE
-    en_US = COALESCE(alina.voc.en_US, VALUES(en_US)),
-    ru_RU = COALESCE(alina.voc.ru_RU, VALUES(ru_RU))
-    ;
+    en_US = COALESCE(alina.voc.en_US, s.en_US),
+    ru_RU = COALESCE(alina.voc.ru_RU, s.ru_RU);
