c7dc246e931d17461ab0646be35c18edff138bb7
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-07-05 04:37:59 +0300

    dev

--- server/srv/alina/Services/Chat/ChatServerRunner.php
+++ server/srv/alina/Services/Chat/ChatServerRunner.php
@@ -8,11 +8,29 @@ use Ratchet\WebSocket\WsServer;
 
 require_once __DIR__ . '../../vendor/autoload.php';
 
-$chatHandler = new ChatServer();
-$webSocket   = new WsServer($chatHandler);
-$httpServer  = new HttpServer($webSocket);
-$socket      = new \React\Socket\SocketServer('0.0.0.0:8080'); // Порт 8080
-$server      = new IoServer($httpServer, $socket);
+class ChatServerRunner
+{
+    public function run()
+    {
+        $chatHandler = new ChatServer();
+        $webSocket   = new WsServer($chatHandler);
+        $httpServer  = new HttpServer($webSocket);
+        $socket      = new \React\Socket\SocketServer('0.0.0.0:8080');
+        $server      = new IoServer($httpServer, $socket);
 
-echo "WebSocket сервер слушает порт 8080...\n";
-$server->run();
+        echo "WebSocket сервер слушает порт 8080...\n";
+        $server->run();
+    }
+
+    public function getSocketState()
+    {
+        return (int) file_get_contents(__DIR__ . '/ChatStateHolder.log');
+    }
+
+    public function setSocketState(int $value)
+    {
+        file_put_contents(__DIR__ . '/ChatStateHolder.log', $value);
+
+        return $this->getSocketState();
+    }
+}
