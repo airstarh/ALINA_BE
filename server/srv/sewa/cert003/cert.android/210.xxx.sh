9fcbb9a6b83999b86c7f3be64e705b47b1f72957
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-07-14 06:16:20 +0300

    draft2

--- server/srv/sewa/cert003/cert.android/210.xxx.sh
+++ server/srv/sewa/cert003/cert.android/210.xxx.sh
@@ -1,9 +1,8 @@
-#! /bin/bash
+#!/bin/bash
+set -e
 
-openssl req -x509 -new -nodes -keyout android_root.key -sha256 -days 1825 -out android_root.crt -subj "/CN=ByRobot Mobile Root CA"
-# 1. Create a request using your original private key and original SAN config
-openssl req -new -key ../cert/030.byrobot.privkey.pem -config ../cert/010.san.conf -out android_server.csr
-# 2. Sign it using the new root CA while preserving the [extensions] section layout
-openssl x509 -req -in android_server.csr -CA android_root.crt -CAkey android_root.key -CAcreateserial -out android_server.crt -days 365 -sha256 -extfile 010.san.conf -extensions extensions
-cat android_server.crt android_root.crt > ../cert/040.byrobot.fullchain.pem
-openssl x509 -in android_root.crt -outform DER -out mobile_trust.crt
+openssl req -x509 -new -nodes -keyout 300.android_root.key -sha256 -days 1825 -out 310.android_root.crt -subj "/CN=ByRobot Mobile Root CA"
+openssl req -new -key ../cert/030.byrobot.privkey.pem -config ../cert/010.san.conf -out 320.android_server.csr
+openssl x509 -req -in 320.android_server.csr -CA 310.android_root.crt -CAkey 300.android_root.key -CAcreateserial -out 330.android_server.crt -days 365 -sha256 -extfile ../cert/010.san.conf -extensions extensions
+cat 330.android_server.crt 310.android_root.crt > ../cert/040.byrobot.fullchain.pem
+openssl x509 -in 310.android_root.crt -outform DER -out 340.mobile_trust.crt
