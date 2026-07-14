ef0e8387b7095e1c7bf430e3c1b95dd920300040
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-07-14 06:12:40 +0300

    dev

--- server/srv/sewa/cert003/cert.android/160.xxx.sh
+++ server/srv/sewa/cert003/cert.android/160.xxx.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
-cat 150.new_server.crt 110.AndroidCA.crt > 170.android.fullchain.pem
+openssl pkcs12 -export \
+  -in ../cert/040.byrobot.fullchain.pem \
+  -inkey ../cert/030.byrobot.privkey.pem \
+  -out 200.zero-home.p12 \
+  -name "zero.home"
+
