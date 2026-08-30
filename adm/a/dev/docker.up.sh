663b41a5fa7972d39241f989cf99a4ea91246d7e
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-08-30 16:37:05 +0300

    dev

--- adm/a/dev/docker.up.sh
+++ adm/a/dev/docker.up.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
 
+sudo bash ./adm/a/dev/perms.sh
+
+docker compose \
+    -f dc.all.yml \
+    -f dc.all.php82.yml \
+    -f dc.all.signal.yml \
+    -f dc.dev.yml \
+    config
+
 docker compose \
     -f dc.all.yml \
     -f dc.all.php82.yml \
     -f dc.all.signal.yml \
     -f dc.dev.yml \
     up -d
+
+sudo bash ./adm/a/dev/perms.sh
