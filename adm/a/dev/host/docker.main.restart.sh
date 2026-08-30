1dfefa3323cf5052754e04f0cf07d1ad00f8397b
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-08-30 14:07:03 +0300

    dev

--- adm/a/dev/host/docker.main.restart.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-. ./adm/a/dev/host/docker.main.down.sh
-sleep 3
-. ./adm/a/dev/host/docker.main.up.sh
--- adm/a/dev/host/docker.main.up.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-docker compose \
-    -f dc.all.yml \
-    -f dc.all.php82.yml \
-    -f dc.all.signal.yml \
-    -f dc.dev.yml \
-    up -d
