d660372d51c4cc3cff778db90507cf7464da02f9
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-08-30 14:07:28 +0300

    dev

--- adm/phpcsfixer/a.fix.sh
+++ adm/phpcsfixer/a.fix.sh
@@ -2,12 +2,15 @@
 
 #! /bin/bash
 
-docker compose -f dc.all.yml -f dc.dev.yml run --rm \
-  -v "$PWD/server/srv/alina/composer.json:/apps/composer.json:ro" \
-  php-cs-fixer fix \
-    server/srv/alina_consumers \
-    server/srv/alina \
-    server/var/www \
-  --config=.php-cs-fixer.dist.php \
-  --cache-file=/tmp/.php-cs-fixer.cache \
-  --allow-risky=yes
+docker compose \
+    -f dc.all.yml \
+    -f dc.dev.yml \
+    run --rm \
+    -v "$PWD/server/srv/alina/composer.json:/apps/composer.json:ro" \
+    php-cs-fixer fix \
+        server/srv/alina_consumers \
+        server/srv/alina \
+        server/var/www \
+        --config=.php-cs-fixer.dist.php \
+        --cache-file=/tmp/.php-cs-fixer.cache \
+        --allow-risky=yes
