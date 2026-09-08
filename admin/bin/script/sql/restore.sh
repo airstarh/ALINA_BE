72b3b620f810991da75ad6f4aee5ef0bb5f5efb7
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-09-08 18:21:26 +0300

    dev

--- admin/bin/script/sql/restore.sh
+++ admin/bin/script/sql/restore.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
-zcat ${A_STORAGE}/${SUB_SQL}/${LOC_DB}.sql.gz | docker exec -i alina_mysql mysql \
-  --binary-mode \
-  -u root \
-  -p"${MYSQL_ROOT_PASSWORD}" \
-  --init-command="SET autocommit=0, unique_checks=0, foreign_key_checks=0;"
+LOC_L_STORE="${A_STORAGE}/${SUB_SQL}"
+BACKUP_FILE="${LOC_L_STORE}/${LOC_DB}.sql.gz"
+
+zcat "${BACKUP_FILE}" \
+    | docker exec -i alina_mysql mysql \
+        --binary-mode \
+        -u root \
+        -p"${MYSQL_ROOT_PASSWORD}" \
+        --init-command="SET autocommit=0, unique_checks=0, foreign_key_checks=0;"
