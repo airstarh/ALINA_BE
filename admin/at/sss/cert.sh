a59ea488b5316602519f5f6c79169e633fac7bd4
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-09-08 21:45:22 +0300

    dev

--- admin/at/sss/cert.sh
+++ admin/at/sss/cert.sh
@@ -14,7 +14,3 @@ docker compose \
   -d ospl1942.ru \
   --cert-name ospl1942.ru \
   --force-renewal
-
-# Uncomment these lines if needed
-# bash ./a.prod.perms.sh
-# docker compose -f dc.all.yml -f dc.prod.yml exec nginx nginx -s reload
