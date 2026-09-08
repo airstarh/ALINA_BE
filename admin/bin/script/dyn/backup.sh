51db3a8a35675397efcc86011c2ca25ec92b7b51
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-09-08 22:07:26 +0300

    dev

---
+++ admin/bin/script/dyn/backup.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+for LOC_PROJECT in "${A_LIST_PROJECTS[@]}"; do
+
+    LOC_SOURCE="${A_R_BE}/${A_R_VAR_WWW}/${LOC_PROJECT}/uploads"
+    LOC_TARGET="${A_L_BE}/${A_L_VAR_WWW}/${LOC_PROJECT}/uploads"
+
+    alina_rsync_from_remote "${LOC_TARGET}" "${LOC_SOURCE}"
+
+done
--- admin/dyn/backup/sss.uploads.dwl.sh
+++ admin/dyn/backup/sss.uploads.dwl.sh
@@ -2,11 +2,4 @@
 
 . ./admin/sss.inc.sh
 
-for LOC_PROJECT in "${A_LIST_PROJECTS[@]}"; do
-
-    LOC_SOURCE="${A_R_BE}/${A_R_VAR_WWW}/${LOC_PROJECT}/uploads"
-    LOC_TARGET="${A_L_BE}/${A_L_VAR_WWW}/${LOC_PROJECT}/uploads"
-
-    alina_rsync_from_remote "${LOC_TARGET}" "${LOC_SOURCE}"
-
-done
+. "$(ALINA_ADMIN)"/bin/script/dyn/backup.sh
