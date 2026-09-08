4523e91580eaa0b6c3f39ab9e8d4529cf0871da3
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-09-08 03:10:15 +0300

    dev

--- admin/sss.inc.sh
+++ admin/sss.inc.sh
@@ -8,6 +8,9 @@ ALINA_ADMIN="${BASH_SOURCE[0]%/*}"
 . "${ALINA_ADMIN}"/bin/function/asd.sh
 ALINA_ADMIN=$(asd)
 
+# ENV
+. $ALINA_ADMIN/../_GITOUT/env/.env
+
 # FUNCTION
 . $ALINA_ADMIN/bin/function/alina_rsync_from_remote.sh
 . $ALINA_ADMIN/bin/function/alina_rsync_local.sh
